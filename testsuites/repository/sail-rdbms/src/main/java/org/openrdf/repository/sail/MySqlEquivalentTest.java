7cf3b80e26fae76639a0ab9ea40e09f9fa700f32
james_leigh <james_leigh@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2008-02-20 19:16:10 +0000

    Add EquivalentTests to testsuite for RDBMS

---
+++ testsuites/repository/sail-rdbms/src/main/java/org/openrdf/repository/sail/MySqlEquivalentTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.repository.sail;
+
+import junit.framework.TestSuite;
+
+import org.openrdf.repository.EquivalentTest;
+import org.openrdf.repository.Repository;
+import org.openrdf.sail.rdbms.mysql.MySqlStore;
+
+
+public class MySqlEquivalentTest extends EquivalentTest {
+
+	public static TestSuite suite() throws Exception {
+		return EquivalentTest.suite(MySqlEquivalentTest.class);
+	}
+
+	public MySqlEquivalentTest() {
+		super();
+	}
+
+	public MySqlEquivalentTest(String name) {
+		super(name);
+	}
+
+	@Override
+	protected Repository newRepository() {
+		return new SailRepository(new MySqlStore("sesame_test"));
+	}
+
+}
