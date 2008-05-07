7953ab59c4b39725e1eb927828ac6d0cf9f0be04
Herko ter Horst <herko.ter.horst@aduna-software.com>
2008-05-07 18:39:28 +0000

    Added some utilities used by Aduna AutoFocus, but deemed to be generally useful.

---
+++ core/repository/manager/src/main/java/org/openrdf/repository/manager/util/RepositoryManagerListener.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.repository.manager.util;
+
+import org.openrdf.repository.manager.RepositoryManager;
+
+public interface RepositoryManagerListener {
+
+	public void initialized(RepositoryManager manager);
+	
+	public void refreshed(RepositoryManager manager);
+	
+	public void shutDown(RepositoryManager manager);
+}
