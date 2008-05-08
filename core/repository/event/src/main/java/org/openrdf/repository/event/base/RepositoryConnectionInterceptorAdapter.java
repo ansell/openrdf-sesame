22564994277108a0aeda34cf97622c225fed92c2
Herko ter Horst <herko.ter.horst@aduna-software.com>
2008-05-08 13:14:52 +0000

    Improved.

---
+++ core/repository/event/src/main/java/org/openrdf/repository/event/base/RepositoryConnectionInterceptorAdapter.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2007.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.repository.event.base;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.event.RepositoryConnectionListener;
+
+/**
+ * @author Herko ter Horst
+ */
+public class RepositoryConnectionInterceptorAdapter implements RepositoryConnectionListener {
+
+	public boolean add(RepositoryConnection conn, Resource subject, URI predicate, Value object,
+			Resource... contexts)
+	{
+		return false;
+	}
+
+	public boolean clear(RepositoryConnection conn, Resource... contexts) {
+		return false;
+	}
+
+	public boolean clearNamespaces(RepositoryConnection conn) {
+		return false;
+	}
+
+	public boolean close(RepositoryConnection conn) {
+		return false;
+	}
+
+	public boolean commit(RepositoryConnection conn) {
+		return false;
+	}
+
+	public boolean remove(RepositoryConnection conn, Resource subject, URI predicate, Value object,
+			Resource... contexts)
+	{
+		return false;
+	}
+
+	public boolean removeNamespace(RepositoryConnection conn, String prefix) {
+		return false;
+	}
+
+	public boolean rollback(RepositoryConnection conn) {
+		return false;
+	}
+
+	public boolean setAutoCommit(RepositoryConnection conn, boolean autoCommit) {
+		return false;
+	}
+
+	public boolean setNamespace(RepositoryConnection conn, String prefix, String name) {
+		return false;
+	}
+}
