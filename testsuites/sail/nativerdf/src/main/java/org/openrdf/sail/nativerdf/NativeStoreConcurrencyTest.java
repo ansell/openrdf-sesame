d012f883bca590d9a51a77a373fd35c9db34838b
Arjohn Kampman <arjohn.kampman@vound-software.com>
2008-05-17 06:42:26 +0000

    Implemented a concurrency test for Sails

---
+++ testsuites/sail/nativerdf/src/main/java/org/openrdf/sail/nativerdf/NativeStoreConcurrencyTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.nativerdf;
+
+import java.io.File;
+
+import info.aduna.io.FileUtil;
+
+import org.openrdf.sail.Sail;
+import org.openrdf.sail.SailConcurrencyTest;
+import org.openrdf.sail.SailException;
+
+/**
+ * An extension of SailConcurrencyTest for testing the class {@link NativeStore}.
+ */
+public class NativeStoreConcurrencyTest extends SailConcurrencyTest {
+
+	/*-----------*
+	 * Variables *
+	 *-----------*/
+
+	private File dataDir;
+
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
+
+	public NativeStoreConcurrencyTest(String name) {
+		super(name);
+	}
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	@Override
+	protected void setUp()
+		throws Exception
+	{
+		dataDir = FileUtil.createTempDir("nativestore");
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown()
+		throws Exception
+	{
+		try {
+			super.tearDown();
+		}
+		finally {
+			FileUtil.deleteDir(dataDir);
+		}
+	}
+
+	@Override
+	protected Sail createSail()
+		throws SailException
+	{
+		return new NativeStore(dataDir, "spoc,posc");
+	}
+}
