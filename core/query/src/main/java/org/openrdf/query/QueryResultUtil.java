c6a7d7bf04573358b8848ffba449011e938a4ad8
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2008-05-22 12:01:06 +0000

    Compare tuple query results directly instead of converting them to a (DAWG format) RDF graph, the latter took ages to complete

--- core/query/src/main/java/org/openrdf/query/QueryResultUtil.java
+++ core/query/src/main/java/org/openrdf/query/QueryResultUtil.java
@@ -5,17 +5,19 @@
  */
 package org.openrdf.query;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import info.aduna.iteration.Iterations;
 
+import org.openrdf.model.BNode;
 import org.openrdf.model.Statement;
 import org.openrdf.model.Value;
 import org.openrdf.model.util.ModelUtil;
-import org.openrdf.query.dawg.DAWGTestResultSetUtil;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
 
@@ -100,10 +102,15 @@ public class QueryResultUtil {
 	public static boolean equals(TupleQueryResult tqr1, TupleQueryResult tqr2)
 		throws QueryEvaluationException
 	{
-		Collection<? extends Statement> graph1 = DAWGTestResultSetUtil.toGraph(tqr1);
-		Collection<? extends Statement> graph2 = DAWGTestResultSetUtil.toGraph(tqr2);
+		List<BindingSet> list1 = Iterations.asList(tqr1);
+		List<BindingSet> list2 = Iterations.asList(tqr2);
 
-		return ModelUtil.equals(graph1, graph2);
+		// Compare the number of statements in both sets
+		if (list1.size() != list2.size()) {
+			return false;
+		}
+
+		return matchBindingSets(list1, list2);
 	}
 
 	public static boolean equals(GraphQueryResult result1, GraphQueryResult result2)
@@ -113,6 +120,110 @@ public class QueryResultUtil {
 		Set<? extends Statement> graph2 = Iterations.asSet(result1);
 
 		return ModelUtil.equals(graph1, graph2);
+	}
+
+	private static boolean matchBindingSets(List<? extends BindingSet> queryResult1,
+			Iterable<? extends BindingSet> queryResult2)
+	{
+		return matchBindingSets(queryResult1, queryResult2, new HashMap<BNode, BNode>(), 0);
+	}
+
+	/**
+	 * A recursive method for finding a complete mapping between blank nodes in
+	 * queryResult1 and blank nodes in queryResult2. The algorithm does a
+	 * depth-first search trying to establish a mapping for each blank node
+	 * occurring in queryResult1.
+	 * 
+	 * @return true if a complete mapping has been found, false otherwise.
+	 */
+	private static boolean matchBindingSets(List<? extends BindingSet> queryResult1,
+			Iterable<? extends BindingSet> queryResult2, Map<BNode, BNode> bNodeMapping, int idx)
+	{
+		boolean result = false;
+
+		if (idx < queryResult1.size()) {
+			BindingSet bs1 = queryResult1.get(idx);
+
+			List<BindingSet> matchingBindingSets = findMatchingBindingSets(bs1, queryResult2, bNodeMapping);
+
+			for (BindingSet bs2 : matchingBindingSets) {
+				// Map bNodes in bs1 to bNodes in bs2
+				Map<BNode, BNode> newBNodeMapping = new HashMap<BNode, BNode>(bNodeMapping);
+
+				for (Binding binding : bs1) {
+					if (binding.getValue() instanceof BNode) {
+						newBNodeMapping.put((BNode)binding.getValue(), (BNode)bs2.getValue(binding.getName()));
+					}
+				}
+
+				// FIXME: this recursive implementation has a high risk of
+				// triggering a stack overflow
+
+				// Enter recursion
+				result = matchBindingSets(queryResult1, queryResult2, newBNodeMapping, idx + 1);
+
+				if (result == true) {
+					// models match, look no further
+					break;
+				}
+			}
+		}
+		else {
+			// All statements have been mapped successfully
+			result = true;
+		}
+
+		return result;
+	}
+
+	private static List<BindingSet> findMatchingBindingSets(BindingSet st,
+			Iterable<? extends BindingSet> model, Map<BNode, BNode> bNodeMapping)
+	{
+		List<BindingSet> result = new ArrayList<BindingSet>();
+
+		for (BindingSet modelSt : model) {
+			if (bindingSetsMatch(st, modelSt, bNodeMapping)) {
+				// All components possibly match
+				result.add(modelSt);
+			}
+		}
+
+		return result;
+	}
+
+	private static boolean bindingSetsMatch(BindingSet bs1, BindingSet bs2, Map<BNode, BNode> bNodeMapping) {
+		for (Binding binding1 : bs1) {
+			Value value1 = binding1.getValue();
+			Value value2 = bs2.getValue(binding1.getName());
+
+			if (value1 instanceof BNode && value2 instanceof BNode) {
+				BNode mappedBNode = bNodeMapping.get(value1);
+
+				if (mappedBNode != null) {
+					// bNode 'value1' was already mapped to some other bNode
+					if (!value2.equals(mappedBNode)) {
+						// 'value1' and 'value2' do not match
+						return false;
+					}
+				}
+				else {
+					// 'value1' was not yet mapped, we need to check if 'value2' is a
+					// possible mapping candidate
+					if (bNodeMapping.containsValue(value2)) {
+						// 'value2' is already mapped to some other value.
+						return false;
+					}
+				}
+			}
+			else {
+				// values are not (both) bNodes
+				if (!value1.equals(value2)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 
 	/**
