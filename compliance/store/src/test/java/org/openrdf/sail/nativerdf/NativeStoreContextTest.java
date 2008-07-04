e396c738c5bdc75f1af75f5f84847c4b975043c3
James Leigh <james@3roundstones.com>
2008-07-04 20:08:07 +0000

    Add native store test for context index

---
+++ compliance/store/src/test/java/org/openrdf/sail/nativerdf/NativeStoreContextTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.nativerdf;
+
+import java.io.File;
+
+import info.aduna.io.FileUtil;
+
+import org.openrdf.sail.NotifyingSail;
+import org.openrdf.sail.RDFNotifyingStoreTest;
+import org.openrdf.sail.SailException;
+
+/**
+ * An extension of RDFStoreTest for testing the class {@link NativeStore}.
+ */
+public class NativeStoreContextTest extends RDFNotifyingStoreTest {
+
+	/*-----------*
+	 * Variables *
+	 *-----------*/
+
+	private File dataDir;
+
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
+
+	public NativeStoreContextTest(String name) {
+		super(name);
+	}
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	@Override
+	protected void setUp()
+		throws Exception
+	{
+		dataDir = FileUtil.createTempDir("nativestore");
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown()
+		throws Exception
+	{
+		super.tearDown();
+		FileUtil.deleteDir(dataDir);
+	}
+
+	@Override
+	protected NotifyingSail createSail()
+		throws SailException
+	{
+		NotifyingSail sail = new NativeStore(dataDir, "cspo");
+		sail.initialize();
+		return sail;
+	}
+}
