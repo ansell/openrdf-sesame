f7b6d305d38e29735546f3c02723d76165c58c92
James Leigh <james@3roundstones.com>
2008-08-26 18:53:44 +0000

    Add copyright notice to java classes

--- core/http/workbench/src/main/java/org/openrdf/workbench/proxy/CookieCacheControlFilter.java
+++ core/http/workbench/src/main/java/org/openrdf/workbench/proxy/CookieCacheControlFilter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
 package org.openrdf.workbench.proxy;
 
 import static java.lang.System.currentTimeMillis;
