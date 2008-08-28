94e3f25502cdc9185eea825206e4a7fa71b695a9
James Leigh <james@3roundstones.com>
2008-08-28 18:32:01 +0000

    Remove *Internal methods from SailConnectionBase

--- core/sail/api/src/main/java/org/openrdf/sail/helpers/SailConnectionBase.java
+++ core/sail/api/src/main/java/org/openrdf/sail/helpers/SailConnectionBase.java
@@ -8,17 +8,8 @@ package org.openrdf.sail.helpers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import info.aduna.iteration.CloseableIteration;
-
 import org.openrdf.StoreException;
-import org.openrdf.model.Namespace;
 import org.openrdf.model.Resource;
-import org.openrdf.model.Statement;
-import org.openrdf.model.URI;
-import org.openrdf.model.Value;
-import org.openrdf.query.BindingSet;
-import org.openrdf.query.Dataset;
-import org.openrdf.query.algebra.TupleExpr;
 import org.openrdf.sail.NotifyingSailConnection;
 import org.openrdf.sail.SailConnection;
 import org.openrdf.sail.inferencer.InferencerConnection;
@@ -59,36 +50,10 @@ public abstract class SailConnectionBase implements SailConnection {
 		return true;
 	}
 
-	public final void close()
+	public void close()
 		throws StoreException
 	{
-		closeInternal();
-	}
-
-	public final CloseableIteration<? extends BindingSet, StoreException> evaluate(
-			TupleExpr tupleExpr, Dataset dataset, BindingSet bindings, boolean includeInferred)
-		throws StoreException
-	{
-		return evaluateInternal(tupleExpr, dataset, bindings, includeInferred);
-	}
-
-	public final CloseableIteration<? extends Resource, StoreException> getContextIDs()
-		throws StoreException
-	{
-		return getContextIDsInternal();
-	}
-
-	public final CloseableIteration<? extends Statement, StoreException> getStatements(Resource subj, URI pred,
-			Value obj, boolean includeInferred, Resource... contexts)
-		throws StoreException
-	{
-		return getStatementsInternal(subj, pred, obj, includeInferred, contexts);
-	}
-
-	public final long size(Resource... contexts)
-		throws StoreException
-	{
-		return sizeInternal(contexts);
+		// no-op
 	}
 
 	protected boolean transactionActive() {
@@ -101,112 +66,21 @@ public abstract class SailConnectionBase implements SailConnection {
 		txnActive = true;
 	}
 
-	public final void commit()
+	public void commit()
 		throws StoreException
 	{
-		commitInternal();
 		txnActive = false;
 	}
 
-	public final void rollback()
+	public void rollback()
 		throws StoreException
 	{
-		rollbackInternal();
 		txnActive = false;
 	}
 
-	public final void addStatement(Resource subj, URI pred, Value obj, Resource... contexts)
+	public void clear(Resource... contexts)
 		throws StoreException
 	{
-		addStatementInternal(subj, pred, obj, contexts);
+		removeStatements(null, null, null, contexts);
 	}
-
-	public final void removeStatements(Resource subj, URI pred, Value obj, Resource... contexts)
-		throws StoreException
-	{
-		removeStatementsInternal(subj, pred, obj, contexts);
-	}
-
-	public final void clear(Resource... contexts)
-		throws StoreException
-	{
-		clearInternal(contexts);
-	}
-
-	public final CloseableIteration<? extends Namespace, StoreException> getNamespaces()
-		throws StoreException
-	{
-		return getNamespacesInternal();
-	}
-
-	public final String getNamespace(String prefix)
-		throws StoreException
-	{
-		return getNamespaceInternal(prefix);
-	}
-
-	public final void setNamespace(String prefix, String name)
-		throws StoreException
-	{
-		setNamespaceInternal(prefix, name);
-	}
-
-	public final void removeNamespace(String prefix)
-		throws StoreException
-	{
-		removeNamespaceInternal(prefix);
-	}
-
-	public final void clearNamespaces()
-		throws StoreException
-	{
-		clearNamespacesInternal();
-	}
-
-	protected abstract void closeInternal()
-		throws StoreException;
-
-	protected abstract CloseableIteration<? extends BindingSet, StoreException> evaluateInternal(
-			TupleExpr tupleExpr, Dataset dataset, BindingSet bindings, boolean includeInferred)
-		throws StoreException;
-
-	protected abstract CloseableIteration<? extends Resource, StoreException> getContextIDsInternal()
-		throws StoreException;
-
-	protected abstract CloseableIteration<? extends Statement, StoreException> getStatementsInternal(
-			Resource subj, URI pred, Value obj, boolean includeInferred, Resource... contexts)
-		throws StoreException;
-
-	protected abstract long sizeInternal(Resource... contexts)
-		throws StoreException;
-
-	protected abstract void commitInternal()
-		throws StoreException;
-
-	protected abstract void rollbackInternal()
-		throws StoreException;
-
-	protected abstract void addStatementInternal(Resource subj, URI pred, Value obj, Resource... contexts)
-		throws StoreException;
-
-	protected abstract void removeStatementsInternal(Resource subj, URI pred, Value obj, Resource... contexts)
-		throws StoreException;
-
-	protected abstract void clearInternal(Resource... contexts)
-		throws StoreException;
-
-	protected abstract CloseableIteration<? extends Namespace, StoreException> getNamespacesInternal()
-		throws StoreException;
-
-	protected abstract String getNamespaceInternal(String prefix)
-		throws StoreException;
-
-	protected abstract void setNamespaceInternal(String prefix, String name)
-		throws StoreException;
-
-	protected abstract void removeNamespaceInternal(String prefix)
-		throws StoreException;
-
-	protected abstract void clearNamespacesInternal()
-		throws StoreException;
 }
