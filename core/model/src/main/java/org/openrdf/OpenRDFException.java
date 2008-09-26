ba6459da9f162e676de30d5c57295077cba4985b
Arjohn Kampman <arjohn.kampman@vound-software.com>
2008-09-26 09:19:35 +0000

    Added serialVersionUID

--- core/model/src/main/java/org/openrdf/OpenRDFException.java
+++ core/model/src/main/java/org/openrdf/OpenRDFException.java
@@ -1,30 +1,32 @@
 /*
- * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2008.
  *
  * Licensed under the Aduna BSD-style license.
  */
 package org.openrdf;
 
-
 /**
- * General superclass of all checked exceptions that parts of OpenRDF Sesame can throw.
+ * General superclass of all checked exceptions that parts of OpenRDF Sesame can
+ * throw.
  * 
  * @author jeen
  */
 public abstract class OpenRDFException extends Exception {
 
+	private static final long serialVersionUID = 8913366826930181397L;
+
 	public OpenRDFException() {
 		super();
 	}
-	
+
 	public OpenRDFException(String msg) {
 		super(msg);
 	}
-	
+
 	public OpenRDFException(Throwable t) {
 		super(t);
 	}
-	
+
 	public OpenRDFException(String msg, Throwable t) {
 		super(msg, t);
 	}
