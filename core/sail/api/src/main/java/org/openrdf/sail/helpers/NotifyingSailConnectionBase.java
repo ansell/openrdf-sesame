ca13fb882f1eb3d56287eec94c0942fe6d29fcc5
Arjohn Kampman <arjohn.kampman@vound-software.com>
2008-10-01 15:06:34 +0000

    Forgot to commit NotifyingSailBase

---
+++ core/sail/api/src/main/java/org/openrdf/sail/helpers/NotifyingSailConnectionBase.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openrdf.model.Statement;
+import org.openrdf.sail.NotifyingSailConnection;
+import org.openrdf.sail.SailConnectionListener;
+
+/**
+ * Abstract Class offering base functionality for SailConnection
+ * implementations.
+ * 
+ * @author Arjohn Kampman
+ * @author jeen
+ */
+public abstract class NotifyingSailConnectionBase extends SailConnectionBase implements
+		NotifyingSailConnection
+{
+
+	/*-----------*
+	 * Variables *
+	 *-----------*/
+
+	private List<SailConnectionListener> listeners;
+
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
+
+	public NotifyingSailConnectionBase(SailBase sailBase) {
+		super(sailBase);
+		listeners = new ArrayList<SailConnectionListener>(0);
+	}
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	public void addConnectionListener(SailConnectionListener listener) {
+		synchronized (listeners) {
+			listeners.add(listener);
+		}
+	}
+
+	public void removeConnectionListener(SailConnectionListener listener) {
+		synchronized (listeners) {
+			listeners.remove(listener);
+		}
+	}
+
+	protected boolean hasConnectionListeners() {
+		synchronized (listeners) {
+			return !listeners.isEmpty();
+		}
+	}
+
+	protected void notifyStatementAdded(Statement st) {
+		synchronized (listeners) {
+			for (SailConnectionListener listener : listeners) {
+				listener.statementAdded(st);
+			}
+		}
+	}
+
+	protected void notifyStatementRemoved(Statement st) {
+		synchronized (listeners) {
+			for (SailConnectionListener listener : listeners) {
+				listener.statementRemoved(st);
+			}
+		}
+	}
+}
