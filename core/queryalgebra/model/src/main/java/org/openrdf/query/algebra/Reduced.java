a4f00ac02e64b5322662daadb97e01802aadfd92
james_leigh <james_leigh@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2008-10-08 15:26:04 +0000

    Add Reduced to the query algebra model

---
+++ core/queryalgebra/model/src/main/java/org/openrdf/query/algebra/Reduced.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.query.algebra;
+
+public class Reduced extends UnaryTupleOperator {
+
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
+
+	public Reduced() {
+	}
+
+	public Reduced(TupleExpr arg) {
+		super(arg);
+	}
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	public <X extends Exception> void visit(QueryModelVisitor<X> visitor)
+		throws X
+	{
+		visitor.meet(this);
+	}
+
+	@Override
+	public Reduced clone() {
+		return (Reduced)super.clone();
+	}
+}
