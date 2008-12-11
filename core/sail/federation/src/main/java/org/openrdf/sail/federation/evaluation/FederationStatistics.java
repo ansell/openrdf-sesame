224c37ed36162a22f8fe61491a4a06ec49ef8930
James Leigh <james@3roundstones.com>
2008-12-11 22:40:13 +0000

    Calculate cardinality from each member in thread pool

--- core/sail/federation/src/main/java/org/openrdf/sail/federation/evaluation/FederationStatistics.java
+++ core/sail/federation/src/main/java/org/openrdf/sail/federation/evaluation/FederationStatistics.java
@@ -5,7 +5,15 @@
  */
 package org.openrdf.sail.federation.evaluation;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +21,12 @@ import org.slf4j.LoggerFactory;
 import org.openrdf.model.Resource;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+import org.openrdf.query.algebra.QueryModel;
 import org.openrdf.query.algebra.StatementPattern;
 import org.openrdf.query.algebra.TupleExpr;
 import org.openrdf.query.algebra.Var;
 import org.openrdf.query.algebra.evaluation.impl.EvaluationStatistics;
+import org.openrdf.query.algebra.helpers.QueryModelVisitorBase;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.store.StoreException;
 
@@ -29,33 +39,24 @@ public class FederationStatistics extends EvaluationStatistics {
 
 	final Logger logger = LoggerFactory.getLogger(FederationStatistics.class);
 
-	private Collection<RepositoryConnection> members;
+	PatternCalculator calculator;
 
-	public FederationStatistics(Collection<RepositoryConnection> members) {
-		this.members = members;
+	public FederationStatistics(Collection<RepositoryConnection> members, QueryModel query) {
+		this.calculator = new PatternCalculator(members, query);
 	}
 
 	public double getCardinality(TupleExpr expr) {
-		Calculator cc = createCardinalityCalculator();
+		Calculator cc = new Calculator();
 		expr.visit(cc);
 		return cc.getCardinality();
-	}
-
-	@Override
-	protected Calculator createCardinalityCalculator() {
-		return new Calculator();
 	}
 
 	protected class Calculator extends CardinalityCalculator {
 
 		@Override
 		protected double getCardinality(StatementPattern sp) {
-			Resource subj = (Resource)getConstantValue(sp.getSubjectVar());
-			URI pred = (URI)getConstantValue(sp.getPredicateVar());
-			Value obj = getConstantValue(sp.getObjectVar());
-			Resource context = (Resource)getConstantValue(sp.getContextVar());
 			try {
-				long cardinality = cardinality(subj, pred, obj, true, context);
+				long cardinality = calculator.getCardinality(sp);
 				double result = cardinality;
 				if (result == Double.NaN || result < 0)
 					return Double.MAX_VALUE;
@@ -66,20 +67,120 @@ public class FederationStatistics extends EvaluationStatistics {
 				return super.getCardinality();
 			}
 		}
+	}
+
+	static class PatternCounter extends QueryModelVisitorBase<RuntimeException> {
+
+		private int count;
+
+		private QueryModel query;
+
+		public PatternCounter(QueryModel query) {
+			this.query = query;
+		}
+
+		public int count() {
+			count = 0;
+			query.visit(this);
+			return count;
+		}
+
+		@Override
+		public void meet(StatementPattern sp) {
+			count++;
+		}
+	}
+
+	static class PatternCalculator extends QueryModelVisitorBase<RuntimeException> {
+
+		static Executor executor = Executors.newCachedThreadPool();
+
+		CountDownLatch latch;
+
+		volatile Exception exception;
+
+		private Collection<RepositoryConnection> members;
+
+		private Map<List<Value>, AtomicLong> cardinalities = new ConcurrentHashMap<List<Value>, AtomicLong>();
+
+		public PatternCalculator(Collection<RepositoryConnection> members, QueryModel query) {
+			this.members = members;
+			int count = new PatternCounter(query).count();
+			this.latch = new CountDownLatch(count * members.size());
+			query.visit(this);
+		}
+
+		public long getCardinality(StatementPattern sp)
+			throws StoreException
+		{
+			await();
+			Resource subj = (Resource)getConstantValue(sp.getSubjectVar());
+			URI pred = (URI)getConstantValue(sp.getPredicateVar());
+			Value obj = getConstantValue(sp.getObjectVar());
+			Resource context = (Resource)getConstantValue(sp.getContextVar());
+			List<Value> key = Arrays.asList(subj, pred, obj, context);
+			return cardinalities.get(key).longValue();
+		}
+
+		@Override
+		public void meet(StatementPattern sp) {
+			final Resource subj = (Resource)getConstantValue(sp.getSubjectVar());
+			final URI pred = (URI)getConstantValue(sp.getPredicateVar());
+			final Value obj = getConstantValue(sp.getObjectVar());
+			final Resource context = (Resource)getConstantValue(sp.getContextVar());
+			List<Value> key = Arrays.asList(subj, pred, obj, context);
+			if (cardinalities.containsKey(key)) {
+				for (int i = 0, n = members.size(); i < n; i++) {
+					latch.countDown(); // run down latch
+				}
+			}
+			else {
+				final AtomicLong cardinality = new AtomicLong(0l);
+				cardinalities.put(key, cardinality);
+				for (final RepositoryConnection member : members) {
+					executor.execute(new Runnable() {
+
+						public void run() {
+							try {
+								long size = member.size(subj, pred, obj, true, context);
+								if (size > 0) {
+									cardinality.getAndAdd(size);
+								}
+							}
+							catch (StoreException e) {
+								exception = e;
+							}
+							finally {
+								latch.countDown();
+							}
+						}
+					});
+				}
+			}
+		}
 
 		private Value getConstantValue(Var var) {
 			return (var != null) ? var.getValue() : null;
 		}
-	}
 
-	long cardinality(Resource subj, URI pred, Value obj, boolean includeInferred, Resource... contexts)
-		throws StoreException
-	{
-		long size = 0;
-		for (RepositoryConnection member : members) {
-			size += member.size(subj, pred, obj, includeInferred, contexts);
+		private void await()
+			throws StoreException
+		{
+			try {
+				latch.await();
+				if (exception != null)
+					throw exception;
+			}
+			catch (StoreException e) {
+				throw e;
+			}
+			catch (RuntimeException e) {
+				throw e;
+			}
+			catch (Exception e) {
+				throw new StoreException(e);
+			}
 		}
-		return size;
 	}
 
 }
