34a6809f33346a6022ec9d6bd5a1b37587ef2dca
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2008-12-12 14:55:35 +0000

    merge from 2.2 branch, rev 7929

--- core/sail/inferencer/src/main/java/org/openrdf/sail/inferencer/fc/DirectTypeHierarchyInferencer.java
+++ core/sail/inferencer/src/main/java/org/openrdf/sail/inferencer/fc/DirectTypeHierarchyInferencer.java
@@ -241,8 +241,8 @@ public class DirectTypeHierarchyInferencer extends NotifyingSailWrapper {
 		public void rollback()
 			throws StoreException
 		{
-			updateNeeded = false;
 			super.rollback();
+			updateNeeded = false;
 		}
 
 		@Override
