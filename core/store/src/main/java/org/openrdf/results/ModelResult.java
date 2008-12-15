948ad84022c9300e98db35eb26b3d1b9917b303c
James Leigh <james@3roundstones.com>
2008-12-15 18:32:26 +0000

    Change GraphResult to be a particular ModelResult (without any context)

--- core/store/src/main/java/org/openrdf/results/ModelResult.java
+++ core/store/src/main/java/org/openrdf/results/ModelResult.java
@@ -5,10 +5,29 @@
  */
 package org.openrdf.results;
 
+import java.util.Map;
+
+import org.openrdf.model.Model;
+import org.openrdf.model.Statement;
+import org.openrdf.repository.RepositoryResult;
+import org.openrdf.store.StoreException;
+
 
 /**
  * @author James Leigh
  */
-public interface ModelResult extends GraphResult {
+public interface ModelResult extends RepositoryResult<Statement> {
+
+	/**
+	 * Retrieves relevant namespaces from the query result.
+	 * 
+	 * @return a Map<String, String> object containing (prefix, namespace) pairs.
+	 * @throws StoreException
+	 */
+	public Map<String, String> getNamespaces()
+		throws StoreException;
+
+	public Model asModel()
+		throws StoreException;
 
 }
