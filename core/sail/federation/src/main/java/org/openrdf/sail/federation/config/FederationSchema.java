c9aa07ded34ecbcbb45f85627ef92ee61f2a7973
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-01-07 10:10:18 +0000

    made constants final

--- core/sail/federation/src/main/java/org/openrdf/sail/federation/config/FederationSchema.java
+++ core/sail/federation/src/main/java/org/openrdf/sail/federation/config/FederationSchema.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008-2009.
  *
  * Licensed under the Aduna BSD-style license.
  */
@@ -16,25 +16,25 @@ import org.openrdf.model.impl.URIImpl;
 public class FederationSchema {
 
 	/** http://www.openrdf.org/config/sail/federation# */
-	public static String NAMESPACE = "http://www.openrdf.org/config/sail/federation#";
+	public static final String NAMESPACE = "http://www.openrdf.org/config/sail/federation#";
 
-	public static URI MEMBER = new URIImpl(NAMESPACE + "member");
+	public static final URI MEMBER = new URIImpl(NAMESPACE + "member");
 
 	/**
 	 * For all triples with a predicate in this space, the container RDF store
 	 * contains all triples with that subject and any predicate in this space.
 	 */
-	public static URI LOCALPROPERTYSPACE = new URIImpl(NAMESPACE + "localPropertySpace");
+	public static final URI LOCALPROPERTYSPACE = new URIImpl(NAMESPACE + "localPropertySpace");
 
 	/**
 	 * If no two members contain the same statement.
 	 */
-	public static URI DISTINCT = new URIImpl(NAMESPACE + "distinct");
+	public static final URI DISTINCT = new URIImpl(NAMESPACE + "distinct");
 
 	/**
 	 * If the federation should not try and add statements to its members.
 	 */
-	public static URI READ_ONLY = new URIImpl(NAMESPACE + "readOnly");
+	public static final URI READ_ONLY = new URIImpl(NAMESPACE + "readOnly");
 
 	private FederationSchema() {
 		// no constructor
