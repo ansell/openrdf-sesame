033b980716f8c60f80abe748175155dcf54a1e57
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-01-07 12:53:49 +0000

    Added copyright headers

--- core/sail/federation/src/main/java/org/openrdf/sail/federation/FederationConnection.java
+++ core/sail/federation/src/main/java/org/openrdf/sail/federation/FederationConnection.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
 package org.openrdf.sail.federation;
 
 import java.util.ArrayList;
