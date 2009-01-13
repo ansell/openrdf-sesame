1e8423571b48e50742a6e12ef5e50a0fdce588b7
James Leigh <james@3roundstones.com>
2009-01-13 14:59:15 +0000

    Replace dataset-repository with dataset-sail

---
+++ core/sail/dataset/src/main/java/org/openrdf/sail/dataset/config/DatasetSchema.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2009.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.dataset.config;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+/**
+ * @author James Leigh
+ */
+public class DatasetSchema {
+
+	/** http://www.openrdf.org/config/sail/dataset# */
+	public static final String NAMESPACE = "http://www.openrdf.org/config/sail/dataset#";
+
+	public static final URI NAMEDGRAPH = new URIImpl(NAMESPACE + "namedGraph");
+
+	public static final URI NAME = new URIImpl(NAMESPACE + "name");
+
+	/**
+	 * Absent dataset indicates the graph is known, but will be loaded
+	 * externally.
+	 */
+	public static final URI DATASET = new URIImpl(NAMESPACE + "dataset");
+
+	/** These are the only datasets that this repository can load. */
+	public static final URI CLOSED = new URIImpl(NAMESPACE + "closed");
+
+	private DatasetSchema() {
+		// no constructor
+	}
+}
