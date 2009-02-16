99e58bdaeadcd3dd1199da244b7123920d3be085
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-02-16 13:40:17 +0000

    Fixed ConcurrentModificationException by returning a CopyOnWriteArrayList in getArgs(); Fixed clone(), which had the side effect of discarding null args

--- core/queryalgebra/model/src/main/java/org/openrdf/query/algebra/NaryOperator.java
+++ core/queryalgebra/model/src/main/java/org/openrdf/query/algebra/NaryOperator.java
@@ -8,9 +8,10 @@ package org.openrdf.query.algebra;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * An abstract superclass for operators which have arguments.
+ * An abstract superclass for operators which have (zero or more) arguments.
  */
 public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryModelNodeBase {
 
@@ -33,14 +34,14 @@ public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryMod
 	}
 
 	/**
-	 * Creates a new nary operator.
+	 * Creates a new n-ary operator.
 	 */
 	public NaryOperator(Expr... args) {
 		setArgs(args);
 	}
 
 	/**
-	 * Creates a new nary operator.
+	 * Creates a new n-ary operator.
 	 */
 	public NaryOperator(Collection<? extends Expr> args) {
 		for (Expr arg : args) {
@@ -53,24 +54,37 @@ public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryMod
 	 *---------*/
 
 	/**
-	 * Gets the arguments of this nary operator.
+	 * Gets the arguments of this n-ary operator.
+	 * 
+	 * @return A copy of the current argument list.
+	 */
+	public List<Expr> getArgs() {
+		return new CopyOnWriteArrayList<Expr>(args);
+	}
+
+	/**
+	 * Gets the number of arguments of this n-ary operator.
+	 * 
+	 * @return The number of arguments.
+	 */
+	public int getNumberOfArguments() {
+		return args.size();
+	}
+
+	/**
+	 * Gets the <tt>idx</tt>-th argument of this n-ary operator.
 	 * 
 	 * @return The operator's arguments.
 	 */
-	public List<Expr> getArgs() {
-		return args;
+	public Expr getArg(int idx) {
+		if (idx >= args.size()) {
+			return null;
+		}
+		return args.get(idx);
 	}
 
 	/**
-	 * Sets the arguments of this nary operator.
-	 */
-	public void setArgs(Expr... args) {
-		this.args.clear();
-		addArgs(args);
-	}
-
-	/**
-	 * Sets the arguments of this nary tuple operator.
+	 * Sets the arguments of this n-ary tuple operator.
 	 */
 	public void addArgs(Expr... args) {
 		assert args != null;
@@ -80,44 +94,38 @@ public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryMod
 	}
 
 	/**
-	 * Sets the arguments of this nary operator.
+	 * Sets the arguments of this n-ary operator.
 	 */
 	public void addArg(Expr arg) {
 		setArg(this.args.size(), arg);
 	}
 
 	/**
-	 * Gets the number of arguments of this nary operator.
-	 * 
-	 * @return The number of arguments.
+	 * Sets the arguments of this n-ary operator.
 	 */
-	public int getNumberOfArguments() {
-		return args.size();
+	public void setArgs(Expr... args) {
+		this.args.clear();
+		addArgs(args);
 	}
 
 	/**
-	 * Gets the <tt>idx</tt>-th argument of this nary operator.
-	 * 
-	 * @return The operator's arguments.
+	 * Sets the <tt>idx</tt>-th argument of this n-ary tuple operator.
 	 */
-	public Expr getArg(int idx) {
-		if (idx >= args.size())
-			return null;
-		return args.get(idx);
-	}
-
-	/**
-	 * Sets the <tt>idx</tt>-th argument of this nary tuple operator.
-	 */
-	public void setArg(int idx, Expr arg) {
+	protected void setArg(int idx, Expr arg) {
 		if (arg != null) {
-			// arg can be null (ie Regex)
+			// arg can be null (i.e. Regex)
 			arg.setParentNode(this);
 		}
+
 		while (args.size() <= idx) {
 			args.add(null);
 		}
+
 		this.args.set(idx, arg);
+	}
+
+	public boolean removeArg(Expr arg) {
+		return args.remove(arg);
 	}
 
 	@Override
@@ -135,16 +143,13 @@ public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryMod
 	@SuppressWarnings("unchecked")
 	public void replaceChildNode(QueryModelNode current, QueryModelNode replacement) {
 		int index = args.indexOf(current);
+
 		if (index >= 0) {
 			setArg(index, (Expr)replacement);
 		}
 		else {
 			super.replaceChildNode(current, replacement);
 		}
-	}
-
-	public boolean removeArg(Expr arg) {
-		return args.remove(arg);
 	}
 
 	@Override
@@ -154,9 +159,8 @@ public abstract class NaryOperator<Expr extends QueryModelNode> extends QueryMod
 
 		clone.args = new ArrayList<Expr>(args.size());
 		for (Expr arg : args) {
-			if (arg != null) {
-				clone.addArg((Expr)arg.clone());
-			}
+			Expr argClone = (arg == null) ? null : (Expr)arg.clone();
+			clone.addArg(argClone);
 		}
 
 		return clone;
