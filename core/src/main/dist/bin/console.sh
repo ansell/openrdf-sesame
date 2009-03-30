e9db3d2686cc2addce2e934f8d3223882a69c144
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-03-30 18:31:12 +0000

    backport of rev 8891: "Add support for directories with spaces"

--- core/src/main/dist/bin/console.sh
+++ core/src/main/dist/bin/console.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 lib="$(dirname "${0}")/../lib"
-java -cp $lib/$(ls "$lib"|xargs |sed "s; ;:$lib/;g") org.openrdf.console.Console
+java -cp "$lib/$(ls "$lib"|xargs |sed "s; ;:$lib/;g")" org.openrdf.console.Console
