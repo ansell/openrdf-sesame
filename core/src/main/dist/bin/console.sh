483e4136d15e241f4c173a79ec0e9e449b873cf6
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-04-29 09:33:41 +0000

    Replaced 'server' script with a 'start-server' and 'stop-server' script combo

--- core/src/main/dist/bin/console.sh
+++ core/src/main/dist/bin/console.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+JAVA_OPT=-mx512m
+
 lib="$(dirname "${0}")/../lib"
-java -cp "$lib/$(ls "$lib"|xargs |sed "s; ;:$lib/;g")" org.openrdf.console.Console $*
+java $JAVA_OPT -cp "$lib/$(ls "$lib"|xargs |sed "s; ;:$lib/;g")" org.openrdf.console.Console $*
--- core/src/main/dist/bin/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-lib="$(dirname "${0}")/../lib"
-java -mx512m -cp "$lib/$(ls "$lib"|xargs |sed "s; ;:$lib/;g")" org.openrdf.http.server.SesameServer $*
