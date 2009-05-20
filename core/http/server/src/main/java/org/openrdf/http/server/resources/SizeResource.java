817ff89af511c8ed0be65d79e2a6f016c585fe98
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-05-20 15:02:12 +0000

    support statements patterns in SizeResource

--- core/http/server/src/main/java/org/openrdf/http/server/resources/SizeResource.java
+++ core/http/server/src/main/java/org/openrdf/http/server/resources/SizeResource.java
@@ -6,9 +6,14 @@
 package org.openrdf.http.server.resources;
 
 import static org.openrdf.http.protocol.Protocol.CONTEXT_PARAM_NAME;
+import static org.openrdf.http.protocol.Protocol.INCLUDE_INFERRED_PARAM_NAME;
+import static org.openrdf.http.protocol.Protocol.OBJECT_PARAM_NAME;
+import static org.openrdf.http.protocol.Protocol.PREDICATE_PARAM_NAME;
+import static org.openrdf.http.protocol.Protocol.SUBJECT_PARAM_NAME;
 import static org.restlet.data.Status.SERVER_ERROR_INTERNAL;
 
 import org.restlet.Context;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
@@ -21,6 +26,8 @@ import org.openrdf.http.server.helpers.ServerConnection;
 import org.openrdf.http.server.helpers.ServerUtil;
 import org.openrdf.http.server.resources.helpers.SesameResource;
 import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.store.StoreException;
 
@@ -29,7 +36,9 @@ import org.openrdf.store.StoreException;
  */
 public class SizeResource extends SesameResource {
 
-	public SizeResource(Context context, Request request, Response response) {
+	public SizeResource(Context context, Request request, Response response)
+		throws ResourceException
+	{
 		super(context, request, response);
 		setNegotiateContent(false);
 		addCacheableVariants(new Variant(MediaType.TEXT_PLAIN));
@@ -44,10 +53,16 @@ public class SizeResource extends SesameResource {
 		if (variant.getMediaType().equals(MediaType.TEXT_PLAIN, true)) {
 			ServerConnection connection = getConnection();
 			ValueFactory vf = connection.getValueFactory();
-			Resource[] contexts = ServerUtil.parseContextParam(this.getQuery(), CONTEXT_PARAM_NAME, vf);
+
+			Form params = getQuery();
+			Resource subj = ServerUtil.parseResourceParam(params, SUBJECT_PARAM_NAME, vf);
+			URI pred = ServerUtil.parseURIParam(params, PREDICATE_PARAM_NAME, vf);
+			Value obj = ServerUtil.parseValueParam(params, OBJECT_PARAM_NAME, vf);
+			Resource[] contexts = ServerUtil.parseContextParam(params, CONTEXT_PARAM_NAME, vf);
+			boolean includeInferred = ServerUtil.parseBooleanParam(params, INCLUDE_INFERRED_PARAM_NAME, true);
 
 			try {
-				long size = connection.size(contexts);
+				long size = connection.sizeMatch(subj, pred, obj, includeInferred, contexts);
 				return new StringRepresentation(String.valueOf(size));
 			}
 			catch (StoreException e) {
