4496b3fcfac3366966fb62ae269cbe847ee3ed9f
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-08-26 07:17:30 +0000

    fixed license header

--- compliance/store/src/test/java/org/openrdf/sail/federation/FederationTest.java
+++ compliance/store/src/test/java/org/openrdf/sail/federation/FederationTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2008.
  *
- * Licensed under the Open Software License version 3.0.
+ * Licensed under the Aduna BSD-style license.
  */
 package org.openrdf.sail.federation;
 
