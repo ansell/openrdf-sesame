c2c5531212b6f8dc875ec4c426102ea7bc8bfbc5
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-09-22 13:05:30 +0000

    fixed method synchronization (merge from 2.3 branch)

--- core/sail/memory/src/main/java/org/openrdf/sail/memory/FileIO.java
+++ core/sail/memory/src/main/java/org/openrdf/sail/memory/FileIO.java
@@ -133,7 +133,7 @@ class FileIO {
 		}
 	}
 
-	private synchronized void write(File dataFile)
+	private void write(File dataFile)
 		throws IOException, StoreException
 	{
 		OutputStream out = new FileOutputStream(dataFile);
@@ -157,7 +157,7 @@ class FileIO {
 		}
 	}
 
-	public void read(File dataFile)
+	public synchronized void read(File dataFile)
 		throws IOException
 	{
 		InputStream in = new FileInputStream(dataFile);
