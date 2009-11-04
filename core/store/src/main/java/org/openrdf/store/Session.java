ca5c20d5b37343da76af3bf4a9a8d230a7faf293
Arjohn Kampman <arjohn.kampman@vound-software.com>
2009-11-04 10:27:41 +0000

    Implemented session basics

---
+++ core/store/src/main/java/org/openrdf/store/Session.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2009.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.store;
+
+/**
+ * Stores thread-based session information. Sessions are typically initialized
+ * in servers.
+ */
+public class Session {
+
+	// TODO: add fields and methods.
+}
