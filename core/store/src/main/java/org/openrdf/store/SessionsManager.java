0f931f287e5ddfee2ea38ec0468e9dabbe0881bc
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-11-04 10:27:41 +0000

    Implemented session basics

---
+++ core/store/src/main/java/org/openrdf/store/SessionsManager.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2009.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.store;
+
+/**
+ * Manages thread-based {@link Session sessions}.
+ */
+public class SessionsManager {
+
+	/**
+	 * The registry for created sessions.
+	 */
+	private static final InheritableThreadLocal<Session> sessions = new InheritableThreadLocal<Session>() {
+
+		@Override
+		protected Session initialValue() {
+			return new Session();
+		}
+	};
+
+	/**
+	 * Gets the session that is associated with the thread that calls this
+	 * method. A new session object will be created if no such session exists
+	 * yet.
+	 * 
+	 * @return The session for the thread that calls this method.
+	 */
+	public static Session get() {
+		return sessions.get();
+	}
+
+	/**
+	 * Removes the session that is associated with the thread that calls this
+	 * method.
+	 */
+	public static void remove() {
+		sessions.remove();
+	}
+}
