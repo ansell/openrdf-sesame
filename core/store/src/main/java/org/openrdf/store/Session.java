2b29502e4499e91fe51c0f25d777b5de980a38ff
Jeen Broekstra <jeen.broekstra@gmail.com>
2009-11-10 13:06:59 +0000

    initial commit of role-based access control implementation. This is just basic setup and layout, code is not yet functional.

--- core/store/src/main/java/org/openrdf/store/Session.java
+++ core/store/src/main/java/org/openrdf/store/Session.java
@@ -5,11 +5,46 @@
  */
 package org.openrdf.store;
 
+import org.openrdf.model.URI;
+
 /**
  * Stores thread-based session information. Sessions are typically initialized
  * in servers.
  */
 public class Session {
 
-	// TODO: add fields and methods.
+	private URI activeRole;
+	private URI currentUser; 
+	
+	/**
+	 * Retrieve the active role of the user
+	 * @return the uri of the current user's active role
+	 */
+	public URI getActiveRole() {
+		return activeRole;
+	}
+	
+	/**
+	 * Sets the user's active role.
+	 * @param activeRole a uri denoting the active role of the user.
+	 */
+	public void setActiveRole(URI activeRole) {
+		this.activeRole = activeRole;
+	}
+	
+	/**
+	 * Retrieve the id of the current user
+	 * @return the id of the current user, as a URI
+	 */
+	public URI getCurrentUser() {
+		return currentUser;
+	}
+	
+	/**
+	 * Sets the id of the current user.
+	 * @param currentUser the id of the current user, as a URI.
+	 */
+	public void setCurrentUser(URI currentUser) {
+		this.currentUser = currentUser;
+	}
 }
