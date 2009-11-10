0de287113609fa2156a3d65f24d09d28f58a5082
jeen <jeen@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-11-10 14:57:39 +0000

    minimal compliance test setup for the access control store. we got greenline :)

---
+++ compliance/store/src/test/java/org/openrdf/sail/accesscontrol/AccessControlTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2009.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.accesscontrol;
+
+import org.openrdf.sail.NotifyingSail;
+import org.openrdf.sail.RDFNotifyingStoreTest;
+import org.openrdf.sail.RDFStoreTest;
+import org.openrdf.sail.Sail;
+import org.openrdf.sail.memory.MemoryStore;
+import org.openrdf.store.StoreException;
+
+
+/**
+ *
+ * @author broek072
+ */
+public class AccessControlTest extends RDFStoreTest {
+
+	/**
+	 * @param name
+	 */
+	public AccessControlTest(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Sail createSail()
+		throws StoreException
+	{
+		Sail sail = new AccessControlSail((Sail)new MemoryStore());
+		sail.initialize();
+		
+		// TODO add some data?
+		
+		return sail;
+	}
+
+
+
+}
