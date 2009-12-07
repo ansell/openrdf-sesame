6d825440aab070bf918a0c6d70fafab69d281c87
jeen <jeen@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-12-07 14:25:07 +0000

    implemented basic security checking on adding and removal of data, plus compliance test (using a backdoor to initialize the store). also added basic SailImplConfig class for AccessControlSail.

---
+++ compliance/store/src/test/java/org/openrdf/sail/accesscontrol/AccessControlSailTest.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2009.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.sail.accesscontrol;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.config.RepositoryConfig;
+import org.openrdf.repository.manager.LocalRepositoryManager;
+import org.openrdf.repository.manager.RepositoryManager;
+import org.openrdf.repository.sail.config.SailRepositoryConfig;
+import org.openrdf.result.TupleResult;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.trig.TriGWriterFactory;
+import org.openrdf.sail.RDFStoreTest;
+import org.openrdf.sail.accesscontrol.config.AccessControlSailConfig;
+import org.openrdf.sail.accesscontrol.vocabulary.ACL;
+import org.openrdf.sail.config.SailImplConfig;
+import org.openrdf.sail.nativerdf.config.NativeStoreConfig;
+import org.openrdf.store.Session;
+import org.openrdf.store.SessionsManager;
+import org.openrdf.store.StoreConfigException;
+import org.openrdf.store.StoreException;
+
+/**
+ * @author Jeen Broekstra
+ */
+public class AccessControlSailTest extends TestCase {
+
+	private static String dataFile = "example-data.ttl";
+
+	private static String policyFile = "policies.ttl";
+
+	private static String resourcePath = "accesscontrol/";
+
+	private ClassLoader cl = AccessControlSailTest.class.getClassLoader();
+
+	private static RepositoryManager manager;
+
+	private static String repositoryId = "test-acl";
+
+	private Repository rep;
+
+	static {
+		File tmpDir = new File("D:/temp/acl-test/");
+		System.out.println("Using repository dir: " + tmpDir.getAbsolutePath());
+		manager = new LocalRepositoryManager(tmpDir);
+		try {
+			manager.initialize();
+		}
+		catch (StoreConfigException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void createNewTestingRepository()
+		throws StoreException, StoreConfigException
+	{
+
+		SailImplConfig sailImplConfig = new NativeStoreConfig();
+		sailImplConfig = new AccessControlSailConfig(sailImplConfig);
+		RepositoryConfig repConfig = new RepositoryConfig(new SailRepositoryConfig(sailImplConfig));
+		manager.addRepositoryConfig(repositoryId, repConfig.export());
+
+		Repository rep = manager.getRepository(repositoryId);
+
+		Session session = SessionsManager.get();
+		
+		// DEBUG 
+		session.setCurrentUser(ACL.ADMIN);
+
+		RepositoryConnection conn = rep.getConnection();
+		try {
+			conn.add(cl.getResource(resourcePath + policyFile), "", RDFFormat.TURTLE, ACL.CONTEXT);
+			conn.add(cl.getResource(resourcePath + dataFile), "", RDFFormat.TURTLE);
+		}
+		catch (RDFParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (StoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		conn.close();
+	}
+
+	protected void setUp()
+		throws Exception
+	{
+		createNewTestingRepository();
+		rep = manager.getRepository(repositoryId);
+		
+		FileOutputStream os = new FileOutputStream("D:/temp/export-acl-test.trig");
+
+		RDFWriter writer = new TriGWriterFactory().getWriter(os);
+		rep.getConnection().export(writer);
+	}
+
+	public void testSimpleQuery()
+		throws Exception
+	{
+
+		RepositoryConnection conn = rep.getConnection();
+
+		try {
+			Session session = SessionsManager.get();
+			session.setCurrentUser(conn.getValueFactory().createURI("http://example.org/bob"));
+
+			String simpleDocumentQuery = "SELECT DISTINCT ?X WHERE {?X a <http://example.org/Document>; ?P ?Y . } ";
+			TupleResult tr = conn.prepareTupleQuery(QueryLanguage.SPARQL, simpleDocumentQuery).evaluate();
+
+			List<String> headers = tr.getBindingNames();
+
+			URI pmdoc1 = conn.getValueFactory().createURI("http://example.org/pmdocument1");
+			URI pmdoc2 = conn.getValueFactory().createURI("http://example.org/pmdocument2");
+
+			while (tr.hasNext()) {
+				BindingSet bs = tr.next();
+
+				for (String header : headers) {
+					Value value = bs.getValue(header);
+					assertNotSame(value, pmdoc1);
+					assertNotSame(value, pmdoc2);
+					System.out.println(header + " = " + value);
+				}
+				System.out.println();
+			}
+		}
+		finally {
+			conn.close();
+		}
+	}
+
+	public void testAddStatement()
+		throws Exception
+	{
+		RepositoryConnection conn = rep.getConnection();
+		try {
+			Session session = SessionsManager.get();
+			session.setCurrentUser(conn.getValueFactory().createURI("http://example.org/bob"));
+			
+			ValueFactory valueFactory = conn.getValueFactory();
+			
+			URI pmdoc1 = conn.getValueFactory().createURI("http://example.org/pmdocument1");
+			URI prdoc1 = conn.getValueFactory().createURI("http://example.org/prdocument1");
+
+			Statement legalStatement = valueFactory.createStatement(prdoc1, RDFS.LABEL, valueFactory.createLiteral("legal new statement"));
+			
+			conn.add(legalStatement);
+
+			Statement illegalStatement = valueFactory.createStatement(pmdoc1, RDFS.LABEL, valueFactory.createLiteral("illegal new statement"));
+			try {
+				conn.add(illegalStatement);
+				
+				fail("should have thrown exception");
+			}
+			catch (StoreException e) {
+				// expected
+				System.out.println(" expected store exception thrown: " + e.getMessage());
+			}
+			
+			
+		}
+		finally {
+			conn.close();
+		}
+	}
+
+}
