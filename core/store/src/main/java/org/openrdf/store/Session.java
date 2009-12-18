61c46af084a0117442925891dbc15df9df5befa2
arjohn <arjohn@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2009-12-18 13:27:33 +0000

    made Session.username volatile

--- core/store/src/main/java/org/openrdf/store/Session.java
+++ core/store/src/main/java/org/openrdf/store/Session.java
@@ -5,28 +5,30 @@
  */
 package org.openrdf.store;
 
-
 /**
  * Stores thread-based session information. Sessions are typically initialized
  * in servers.
  */
 public class Session {
 
-	private String username; 
-	
+	private volatile String username;
+
 	/**
 	 * Retrieve the username of the current user
+	 * 
 	 * @return the username of the current user
 	 */
 	public String getUsername() {
 		return username;
 	}
-	
+
 	/**
 	 * Sets the username of the current user.
-	 * @param currentUser the username of the current user, as a URI.
+	 * 
+	 * @param username
+	 *        the username of the current user, as a URI.
 	 */
-	public void setUsername(String currentUser) {
-		this.username = currentUser;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 }
