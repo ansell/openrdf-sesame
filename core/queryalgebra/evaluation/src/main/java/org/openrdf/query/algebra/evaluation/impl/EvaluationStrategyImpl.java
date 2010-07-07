f879534267f6f76a6b4e72ee4bbda14c3cb28335
Arjohn Kampman <arjohn.kampman@vound-software.com>
2010-07-07 14:32:04 +0000

    Fixed [SES-713] Query with strings in a maths expression results in an AssertionError during evaluation

--- core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/impl/EvaluationStrategyImpl.java
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/impl/EvaluationStrategyImpl.java
@@ -1237,7 +1237,7 @@ public class EvaluationStrategyImpl implements EvaluationStrategy {
 			return getValue((Literal)leftVal, (Literal)rightVal, node.getOperator());
 		}
 
-		return null;
+		throw new ValueExprEvaluationException("Both arguments must be numeric literals");
 	}
 
 	public Value evaluate(In node, BindingSet bindings)
@@ -1360,123 +1360,126 @@ public class EvaluationStrategyImpl implements EvaluationStrategy {
 		}
 	}
 
-	private static Literal getValue(Literal leftLit, Literal rightLit, MathOp op) {
+	private static Literal getValue(Literal leftLit, Literal rightLit, MathOp op)
+		throws ValueExprEvaluationException
+	{
 		URI leftDatatype = leftLit.getDatatype();
 		URI rightDatatype = rightLit.getDatatype();
 
 		// Only numeric value can be used in math expressions
-		if (leftDatatype != null && rightDatatype != null && XMLDatatypeUtil.isNumericDatatype(leftDatatype)
-				&& XMLDatatypeUtil.isNumericDatatype(rightDatatype))
-		{
-			// Determine most specific datatype that the arguments have in common,
-			// choosing from xsd:integer, xsd:decimal, xsd:float and xsd:double as
-			// per the SPARQL/XPATH spec
-			URI commonDatatype;
-
-			if (leftDatatype.equals(XMLSchema.DOUBLE) || rightDatatype.equals(XMLSchema.DOUBLE)) {
-				commonDatatype = XMLSchema.DOUBLE;
-			}
-			else if (leftDatatype.equals(XMLSchema.FLOAT) || rightDatatype.equals(XMLSchema.FLOAT)) {
-				commonDatatype = XMLSchema.FLOAT;
-			}
-			else if (leftDatatype.equals(XMLSchema.DECIMAL) || rightDatatype.equals(XMLSchema.DECIMAL)) {
-				commonDatatype = XMLSchema.DECIMAL;
-			}
-			else if (op == MathOp.DIVIDE) {
-				// Result of integer divide is decimal and requires the arguments to
-				// be handled as such, see for details:
-				// http://www.w3.org/TR/xpath-functions/#func-numeric-divide
-				commonDatatype = XMLSchema.DECIMAL;
-			}
-			else {
-				commonDatatype = XMLSchema.INTEGER;
-			}
-
-			// Note: Java already handles cases like divide-by-zero appropriately
-			// for floats and doubles, see:
-			// http://www.particle.kth.se/~lindsey/JavaCourse/Book/Part1/Tech/
-			// Chapter02/floatingPt2.html
-
-			try {
-				if (commonDatatype.equals(XMLSchema.DOUBLE)) {
-					double left = leftLit.doubleValue();
-					double right = rightLit.doubleValue();
-
-					switch (op) {
-						case PLUS:
-							return new NumericLiteralImpl(left + right);
-						case MINUS:
-							return new NumericLiteralImpl(left - right);
-						case MULTIPLY:
-							return new NumericLiteralImpl(left * right);
-						case DIVIDE:
-							return new NumericLiteralImpl(left / right);
-						default:
-							throw new IllegalArgumentException("Unknown operator: " + op);
-					}
-				}
-				else if (commonDatatype.equals(XMLSchema.FLOAT)) {
-					float left = leftLit.floatValue();
-					float right = rightLit.floatValue();
-
-					switch (op) {
-						case PLUS:
-							return new NumericLiteralImpl(left + right);
-						case MINUS:
-							return new NumericLiteralImpl(left - right);
-						case MULTIPLY:
-							return new NumericLiteralImpl(left * right);
-						case DIVIDE:
-							return new NumericLiteralImpl(left / right);
-						default:
-							throw new IllegalArgumentException("Unknown operator: " + op);
-					}
-				}
-				else if (commonDatatype.equals(XMLSchema.DECIMAL)) {
-					BigDecimal left = leftLit.decimalValue();
-					BigDecimal right = rightLit.decimalValue();
-
-					switch (op) {
-						case PLUS:
-							return new DecimalLiteralImpl(left.add(right));
-						case MINUS:
-							return new DecimalLiteralImpl(left.subtract(right));
-						case MULTIPLY:
-							return new DecimalLiteralImpl(left.multiply(right));
-						case DIVIDE:
-							// Divide by zero handled through NumberFormatException
-							return new DecimalLiteralImpl(left.divide(right, RoundingMode.HALF_UP));
-						default:
-							throw new IllegalArgumentException("Unknown operator: " + op);
-					}
-				}
-				else { // XMLSchema.INTEGER
-					BigInteger left = leftLit.integerValue();
-					BigInteger right = rightLit.integerValue();
-
-					switch (op) {
-						case PLUS:
-							return new IntegerLiteralImpl(left.add(right));
-						case MINUS:
-							return new IntegerLiteralImpl(left.subtract(right));
-						case MULTIPLY:
-							return new IntegerLiteralImpl(left.multiply(right));
-						case DIVIDE:
-							throw new RuntimeException("Integer divisions should be processed as decimal divisions");
-						default:
-							throw new IllegalArgumentException("Unknown operator: " + op);
-					}
-				}
-			}
-			catch (NumberFormatException e) {
-				return null;
-			}
-			catch (ArithmeticException e) {
-				return null;
-			}
+		if (leftDatatype == null || !XMLDatatypeUtil.isNumericDatatype(leftDatatype)) {
+			throw new ValueExprEvaluationException("Not a number: " + leftLit);
+		}
+		if (rightDatatype == null || !XMLDatatypeUtil.isNumericDatatype(rightDatatype)) {
+			throw new ValueExprEvaluationException("Not a number: " + rightLit);
 		}
 
-		return null;
+		// Determine most specific datatype that the arguments have in common,
+		// choosing from xsd:integer, xsd:decimal, xsd:float and xsd:double as
+		// per the SPARQL/XPATH spec
+		URI commonDatatype;
+
+		if (leftDatatype.equals(XMLSchema.DOUBLE) || rightDatatype.equals(XMLSchema.DOUBLE)) {
+			commonDatatype = XMLSchema.DOUBLE;
+		}
+		else if (leftDatatype.equals(XMLSchema.FLOAT) || rightDatatype.equals(XMLSchema.FLOAT)) {
+			commonDatatype = XMLSchema.FLOAT;
+		}
+		else if (leftDatatype.equals(XMLSchema.DECIMAL) || rightDatatype.equals(XMLSchema.DECIMAL)) {
+			commonDatatype = XMLSchema.DECIMAL;
+		}
+		else if (op == MathOp.DIVIDE) {
+			// Result of integer divide is decimal and requires the arguments to
+			// be handled as such, see for details:
+			// http://www.w3.org/TR/xpath-functions/#func-numeric-divide
+			commonDatatype = XMLSchema.DECIMAL;
+		}
+		else {
+			commonDatatype = XMLSchema.INTEGER;
+		}
+
+		// Note: Java already handles cases like divide-by-zero appropriately
+		// for floats and doubles, see:
+		// http://www.particle.kth.se/~lindsey/JavaCourse/Book/Part1/Tech/
+		// Chapter02/floatingPt2.html
+
+		try {
+			if (commonDatatype.equals(XMLSchema.DOUBLE)) {
+				double left = leftLit.doubleValue();
+				double right = rightLit.doubleValue();
+
+				switch (op) {
+					case PLUS:
+						return new NumericLiteralImpl(left + right);
+					case MINUS:
+						return new NumericLiteralImpl(left - right);
+					case MULTIPLY:
+						return new NumericLiteralImpl(left * right);
+					case DIVIDE:
+						return new NumericLiteralImpl(left / right);
+					default:
+						throw new IllegalArgumentException("Unknown operator: " + op);
+				}
+			}
+			else if (commonDatatype.equals(XMLSchema.FLOAT)) {
+				float left = leftLit.floatValue();
+				float right = rightLit.floatValue();
+
+				switch (op) {
+					case PLUS:
+						return new NumericLiteralImpl(left + right);
+					case MINUS:
+						return new NumericLiteralImpl(left - right);
+					case MULTIPLY:
+						return new NumericLiteralImpl(left * right);
+					case DIVIDE:
+						return new NumericLiteralImpl(left / right);
+					default:
+						throw new IllegalArgumentException("Unknown operator: " + op);
+				}
+			}
+			else if (commonDatatype.equals(XMLSchema.DECIMAL)) {
+				BigDecimal left = leftLit.decimalValue();
+				BigDecimal right = rightLit.decimalValue();
+
+				switch (op) {
+					case PLUS:
+						return new DecimalLiteralImpl(left.add(right));
+					case MINUS:
+						return new DecimalLiteralImpl(left.subtract(right));
+					case MULTIPLY:
+						return new DecimalLiteralImpl(left.multiply(right));
+					case DIVIDE:
+						// Divide by zero handled through NumberFormatException
+						return new DecimalLiteralImpl(left.divide(right, RoundingMode.HALF_UP));
+					default:
+						throw new IllegalArgumentException("Unknown operator: " + op);
+				}
+			}
+			else { // XMLSchema.INTEGER
+				BigInteger left = leftLit.integerValue();
+				BigInteger right = rightLit.integerValue();
+
+				switch (op) {
+					case PLUS:
+						return new IntegerLiteralImpl(left.add(right));
+					case MINUS:
+						return new IntegerLiteralImpl(left.subtract(right));
+					case MULTIPLY:
+						return new IntegerLiteralImpl(left.multiply(right));
+					case DIVIDE:
+						throw new RuntimeException("Integer divisions should be processed as decimal divisions");
+					default:
+						throw new IllegalArgumentException("Unknown operator: " + op);
+				}
+			}
+		}
+		catch (NumberFormatException e) {
+			throw new ValueExprEvaluationException(e);
+		}
+		catch (ArithmeticException e) {
+			throw new ValueExprEvaluationException(e);
+		}
 	}
 
 	private boolean isReduced(QueryModelNode node) {
