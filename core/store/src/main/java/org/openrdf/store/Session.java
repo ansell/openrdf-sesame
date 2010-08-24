b5eb280a449f4a2f9657f08b715d022370f34b70
jeen <jeen@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2010-08-24 06:43:47 +0000

    fix in handling of caching of user id. cached user id is now stored in Session object itself, and reset as a side-effect of setting a username.

--- core/store/src/main/java/org/openrdf/store/Session.java
+++ core/store/src/main/java/org/openrdf/store/Session.java
@@ -5,6 +5,8 @@
  */
 package org.openrdf.store;
 
+import org.openrdf.model.URI;
+
 /**
  * Stores thread-based session information. Sessions are typically initialized
  * in servers.
@@ -12,6 +14,8 @@ package org.openrdf.store;
 public class Session {
 
 	private volatile String username;
+	
+	private volatile URI userId;
 	
 	/**
 	 * Retrieve the username of the current user
@@ -23,12 +27,32 @@ public class Session {
 	}
 
 	/**
-	 * Sets the username of the current user.
+	 * Sets the username of the current user. As a side-effect the user ID is reset.
 	 * 
 	 * @param username
-	 *        the username of the current user, as a URI.
+	 *        the username of the current user.
 	 */
 	public void setUsername(String username) {
 		this.username = username;
+		setUserId(null);
 	}
+
+	/**
+	 * Sets the User ID of the current user. For internal use only.
+	 * 
+	 * @param userId
+	 */
+	public void setUserId(URI userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * Retrieve the User ID of the current user. 
+	 * 
+	 * @return a URI representing the user ID of the current user, or null.
+	 */
+	public URI getUserId() {
+		return userId;
+	}
+	
 }
