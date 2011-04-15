dd4cc9238fdd8ff29be5f3dae0cd5db60e8220a7
jeen <jeen@3fbd4d3e-0f96-47c4-bab7-89622e02a19e>
2011-04-15 00:14:14 +0000

    SES-755 implemented workaround for PostgreSQL JDBC driver bug: it does not accept Long.MAX_VALUE as a valid bigint. Introduced PgSqlQueryBuilder class which override the creation of the SQL query string, using PgSql's "LIMIT ALL" syntax in case the limit is set to MAX_VALUE.

--- core/sail/rdbms/src/main/java/org/openrdf/sail/rdbms/postgresql/PgQueryBuilderFactory.java
+++ core/sail/rdbms/src/main/java/org/openrdf/sail/rdbms/postgresql/PgQueryBuilderFactory.java
@@ -7,17 +7,24 @@ package org.openrdf.sail.rdbms.postgresql;
 
 import org.openrdf.sail.rdbms.evaluation.QueryBuilderFactory;
 import org.openrdf.sail.rdbms.evaluation.SqlExprBuilder;
+import org.openrdf.sail.rdbms.evaluation.SqlQueryBuilder;
 import org.openrdf.sail.rdbms.evaluation.SqlRegexBuilder;
 
 /**
  * Overrides PostgreSQL specific SQL syntax. Including regular expression
- * operator and CROSS JOIN notation.
+ * operator, LIMIT clause and CROSS JOIN notation.
  * 
+ * @author Jeen Broekstra
  * @author James Leigh
  * 
  */
 public class PgQueryBuilderFactory extends QueryBuilderFactory {
 
+	@Override 
+	public SqlQueryBuilder createSqlQueryBuilder() {
+		return new PgSqlQueryBuilder(this);
+	}
+	
 	@Override
 	public SqlRegexBuilder createSqlRegexBuilder(SqlExprBuilder where) {
 		return new SqlRegexBuilder(where, this) {
