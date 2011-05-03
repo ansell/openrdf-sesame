9bdf38e0ee5f31f432ea6ebc9e51e4ec772a8273
Arjohn Kampman <arjohn.kampman@vound-software.com>
2011-05-03 18:02:11 +0000

    Implemented [SES-769] Make cache sizes of native store's ValueStore configurable

--- core/sail/nativerdf/src/main/java/org/openrdf/sail/nativerdf/config/NativeStoreConfig.java
+++ core/sail/nativerdf/src/main/java/org/openrdf/sail/nativerdf/config/NativeStoreConfig.java
@@ -6,11 +6,16 @@
 package org.openrdf.sail.nativerdf.config;
 
 import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.FORCE_SYNC;
+import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.NAMESPACE_CACHE_SIZE;
+import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.NAMESPACE_ID_CACHE_SIZE;
 import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.TRIPLE_INDEXES;
+import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.VALUE_CACHE_SIZE;
+import static org.openrdf.sail.nativerdf.config.NativeStoreSchema.VALUE_ID_CACHE_SIZE;
 
 import org.openrdf.model.Graph;
 import org.openrdf.model.Literal;
 import org.openrdf.model.Resource;
+import org.openrdf.model.ValueFactory;
 import org.openrdf.model.util.GraphUtil;
 import org.openrdf.model.util.GraphUtilException;
 import org.openrdf.sail.config.SailConfigException;
@@ -28,6 +33,14 @@ public class NativeStoreConfig extends SailImplConfigBase {
 	private String tripleIndexes;
 
 	private boolean forceSync = false;
+
+	private int valueCacheSize = -1;
+
+	private int valueIDCacheSize = -1;
+
+	private int namespaceCacheSize = -1;
+
+	private int namespaceIDCacheSize = -1;
 
 	/*--------------*
 	 * Constructors *
@@ -67,16 +80,60 @@ public class NativeStoreConfig extends SailImplConfigBase {
 		this.forceSync = forceSync;
 	}
 
+	public int getValueCacheSize() {
+		return valueCacheSize;
+	}
+
+	public void setValueCacheSize(int valueCacheSize) {
+		this.valueCacheSize = valueCacheSize;
+	}
+
+	public int getValueIDCacheSize() {
+		return valueIDCacheSize;
+	}
+
+	public void setValueIDCacheSize(int valueIDCacheSize) {
+		this.valueIDCacheSize = valueIDCacheSize;
+	}
+
+	public int getNamespaceCacheSize() {
+		return namespaceCacheSize;
+	}
+
+	public void setNamespaceCacheSize(int namespaceCacheSize) {
+		this.namespaceCacheSize = namespaceCacheSize;
+	}
+
+	public int getNamespaceIDCacheSize() {
+		return namespaceIDCacheSize;
+	}
+
+	public void setNamespaceIDCacheSize(int namespaceIDCacheSize) {
+		this.namespaceIDCacheSize = namespaceIDCacheSize;
+	}
+
 	@Override
-	public Resource export(Graph graph)
-	{
+	public Resource export(Graph graph) {
 		Resource implNode = super.export(graph);
+		ValueFactory vf = graph.getValueFactory();
 
 		if (tripleIndexes != null) {
-			graph.add(implNode, TRIPLE_INDEXES, graph.getValueFactory().createLiteral(tripleIndexes));
+			graph.add(implNode, TRIPLE_INDEXES, vf.createLiteral(tripleIndexes));
 		}
 		if (forceSync) {
-			graph.add(implNode, FORCE_SYNC, graph.getValueFactory().createLiteral(forceSync));
+			graph.add(implNode, FORCE_SYNC, vf.createLiteral(forceSync));
+		}
+		if (valueCacheSize >= 0) {
+			graph.add(implNode, VALUE_CACHE_SIZE, vf.createLiteral(valueCacheSize));
+		}
+		if (valueIDCacheSize >= 0) {
+			graph.add(implNode, VALUE_ID_CACHE_SIZE, vf.createLiteral(valueIDCacheSize));
+		}
+		if (namespaceCacheSize >= 0) {
+			graph.add(implNode, NAMESPACE_CACHE_SIZE, vf.createLiteral(namespaceCacheSize));
+		}
+		if (namespaceIDCacheSize >= 0) {
+			graph.add(implNode, NAMESPACE_ID_CACHE_SIZE, vf.createLiteral(namespaceIDCacheSize));
 		}
 
 		return implNode;
@@ -102,6 +159,53 @@ public class NativeStoreConfig extends SailImplConfigBase {
 				catch (IllegalArgumentException e) {
 					throw new SailConfigException("Boolean value required for " + FORCE_SYNC + " property, found "
 							+ forceSyncLit);
+				}
+			}
+
+			Literal valueCacheSizeLit = GraphUtil.getOptionalObjectLiteral(graph, implNode, VALUE_CACHE_SIZE);
+			if (valueCacheSizeLit != null) {
+				try {
+					setValueCacheSize(valueCacheSizeLit.intValue());
+				}
+				catch (NumberFormatException e) {
+					throw new SailConfigException("Integer value required for " + VALUE_CACHE_SIZE
+							+ " property, found " + valueCacheSizeLit);
+				}
+			}
+
+			Literal valueIDCacheSizeLit = GraphUtil.getOptionalObjectLiteral(graph, implNode,
+					VALUE_ID_CACHE_SIZE);
+			if (valueIDCacheSizeLit != null) {
+				try {
+					setValueIDCacheSize(valueIDCacheSizeLit.intValue());
+				}
+				catch (NumberFormatException e) {
+					throw new SailConfigException("Integer value required for " + VALUE_ID_CACHE_SIZE
+							+ " property, found " + valueIDCacheSizeLit);
+				}
+			}
+
+			Literal namespaceCacheSizeLit = GraphUtil.getOptionalObjectLiteral(graph, implNode,
+					NAMESPACE_CACHE_SIZE);
+			if (namespaceCacheSizeLit != null) {
+				try {
+					setNamespaceCacheSize(namespaceCacheSizeLit.intValue());
+				}
+				catch (NumberFormatException e) {
+					throw new SailConfigException("Integer value required for " + NAMESPACE_CACHE_SIZE
+							+ " property, found " + namespaceCacheSizeLit);
+				}
+			}
+
+			Literal namespaceIDCacheSizeLit = GraphUtil.getOptionalObjectLiteral(graph, implNode,
+					NAMESPACE_ID_CACHE_SIZE);
+			if (namespaceIDCacheSizeLit != null) {
+				try {
+					setNamespaceIDCacheSize(namespaceIDCacheSizeLit.intValue());
+				}
+				catch (NumberFormatException e) {
+					throw new SailConfigException("Integer value required for " + NAMESPACE_ID_CACHE_SIZE
+							+ " property, found " + namespaceIDCacheSizeLit);
 				}
 			}
 		}
