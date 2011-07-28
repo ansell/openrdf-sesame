f88994b7836f56a8e03531c936d05da9c72941b2
Jeen Broekstra <jeen.broekstra@gmail.com>
2011-07-28 23:57:58 +0000

    SES-796 refactored function subpackages by argument type, and using XPath function URIs for naming of appropriate functions.

--- core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/string/StrStarts.java
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/string/StrStarts.java
@@ -3,13 +3,14 @@
  *
  * Licensed under the Aduna BSD-style license.
  */
-package org.openrdf.query.algebra.evaluation.function.builtin;
+package org.openrdf.query.algebra.evaluation.function.string;
 
 import org.openrdf.model.Literal;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.BooleanLiteralImpl;
+import org.openrdf.model.vocabulary.FN;
 import org.openrdf.query.algebra.evaluation.ValueExprEvaluationException;
 import org.openrdf.query.algebra.evaluation.function.Function;
 import org.openrdf.query.algebra.evaluation.util.QueryEvaluationUtil;
@@ -24,7 +25,7 @@ import org.openrdf.query.algebra.evaluation.util.QueryEvaluationUtil;
 public class StrStarts implements Function {
 
 	public String getURI() {
-		return "STRSTARTS";
+		return FN.STARTS_WITH.toString();
 	}
 
 	public Literal evaluate(ValueFactory valueFactory, Value... args)
