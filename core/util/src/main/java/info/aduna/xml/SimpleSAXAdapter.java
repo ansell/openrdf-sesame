ef5e1f3720b2b458caffbd50f5def2b8654048aa
Arjohn Kampman <arjohn.kampman@vound-software.com>
2011-11-07 14:50:08 +0000

    [SES-874] copies necessary classes from aduna-commons-xml and aduna-commons-text to sesame-util

---
+++ core/util/src/main/java/info/aduna/xml/SimpleSAXAdapter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+ 
+package info.aduna.xml;
+
+import java.util.Map;
+
+import org.xml.sax.SAXException;
+
+/**
+ * An implementation of <tt>SimpleSAXListener</tt> providing dummy
+ * implementations for all its methods.
+ */
+public class SimpleSAXAdapter implements SimpleSAXListener {
+
+	// implements SimpleSAXListener.startDocument()
+	public void startDocument()
+		throws SAXException
+	{
+	}
+
+	// implements SimpleSAXListener.endDocument()
+	public void endDocument()
+		throws SAXException
+	{
+	}
+
+	// implements SimpleSAXListener.startTag()
+	public void startTag(String tagName, Map<String, String> atts, String text)
+		throws SAXException
+	{
+	}
+
+	// implements SimpleSAXListener.endTag()
+	public void endTag(String tagName)
+		throws SAXException
+	{
+	}
+}
