b592b06d474d224703aabcdabc0d195378cedb29
Jeen Broekstra <jeen.broekstra@gmail.com>
2011-11-17 02:27:37 +0000

    SES-885 added impl of STRBEFORE. needs more testing on datatype/language handling and needs to be integrated in parser.

---
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/string/StrBefore.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2011.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.query.algebra.evaluation.function.string;
+
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.vocabulary.FN;
+import org.openrdf.model.vocabulary.XMLSchema;
+import org.openrdf.query.algebra.evaluation.ValueExprEvaluationException;
+import org.openrdf.query.algebra.evaluation.function.Function;
+
+/**
+ * The SPARQL built-in {@link Function} STRBEFORE, as defined in <a
+ * href="http://www.w3.org/TR/sparql11-query/#func-substr">SPARQL Query Language
+ * for RDF</a>.
+ * 
+ * @author Jeen Broekstra
+ */
+public class StrBefore implements Function {
+
+	public String getURI() {
+		return FN.SUBSTRING_BEFORE.toString();
+	}
+
+	public Literal evaluate(ValueFactory valueFactory, Value... args)
+		throws ValueExprEvaluationException
+	{
+		if (args.length != 2) {
+			throw new ValueExprEvaluationException("Incorrect number of arguments for STRBEFORE: " + args.length);
+		}
+
+		Value leftArg = args[0];
+		Value rightArg = args[1];
+
+		if (leftArg instanceof Literal && rightArg instanceof Literal) {
+			Literal leftLit = (Literal)leftArg;
+			Literal rightLit = (Literal)rightArg;
+
+			String leftLanguage = leftLit.getLanguage();
+			String rightLanguage = rightLit.getLanguage();
+			
+			URI leftDt = leftLit.getDatatype();
+			URI rightDt = rightLit.getDatatype();
+			
+			// STRBEFORE function accepts only plain literals (optionally
+			// language-tagged) or string-typed literals.
+			if ((leftLanguage != null
+					|| (leftDt == null || XMLSchema.STRING.equals(leftDt)))
+					&& (rightLanguage == null || rightLanguage.equals(leftLanguage))
+					&& (rightDt == null || XMLSchema.STRING.equals(rightDt)))
+			{
+				String lexicalValue = leftLit.getLabel();
+				String substring = rightLit.getLabel();
+				
+				int index = lexicalValue.indexOf(substring);
+
+				String subBefore = "";
+				if (index > -1) {
+					subBefore = lexicalValue.substring(0, index);
+				}
+				return valueFactory.createLiteral(subBefore);
+			}
+			else {
+				throw new ValueExprEvaluationException("incompatible operands: " + leftArg + ", " + rightArg);
+			}
+		}
+		else {
+			throw new ValueExprEvaluationException("incompatible operands: " + leftArg + ", " + rightArg);
+		}
+	}
+}
