dd1207a9c16013d14ef250fc5f8a69171a33414d
Jeen Broekstra <jeen.broekstra@gmail.com>
2012-03-04 21:57:17 +0000

    SES-944 argument compatibility checking inlined with corrected definitions of string literal and simple literal.

--- core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/DecimalCast.java
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/DecimalCast.java
@@ -39,7 +39,7 @@ public class DecimalCast implements Function {
 			Literal literal = (Literal)args[0];
 			URI datatype = literal.getDatatype();
 
-			if (QueryEvaluationUtil.isSimpleLiteral(literal)) {
+			if (QueryEvaluationUtil.isStringLiteral(literal)) {
 				String decimalValue = XMLDatatypeUtil.collapseWhiteSpace(literal.getLabel());
 				if (XMLDatatypeUtil.isValidDecimal(decimalValue)) {
 					return valueFactory.createLiteral(decimalValue, XMLSchema.DECIMAL);
--- core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/IntegerCast.java
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/IntegerCast.java
@@ -39,7 +39,7 @@ public class IntegerCast implements Function {
 			Literal literal = (Literal)args[0];
 			URI datatype = literal.getDatatype();
 
-			if (QueryEvaluationUtil.isSimpleLiteral(literal)) {
+			if (QueryEvaluationUtil.isStringLiteral(literal)) {
 				String integerValue = XMLDatatypeUtil.collapseWhiteSpace(literal.getLabel());
 				if (XMLDatatypeUtil.isValidInteger(integerValue)) {
 					return valueFactory.createLiteral(integerValue, XMLSchema.INTEGER);
