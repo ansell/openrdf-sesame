b54317e8d9f5fe4da05158160d45c07e26f9acf0
Jeen Broekstra <jeen.broekstra@gmail.com>
2012-09-11 23:35:44 +0000

    SES-1071 disabled all MySQL/PostgreSQL related conformance tests.

--- compliance/store/src/test/java/org/openrdf/repository/sail/rdbms/PgSqlSparqlOrderByTest.java
+++ compliance/store/src/test/java/org/openrdf/repository/sail/rdbms/PgSqlSparqlOrderByTest.java
@@ -1,21 +1,22 @@
-/*
- * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
- *
- * Licensed under the Aduna BSD-style license.
- */
-package org.openrdf.repository.sail.rdbms;
-
-import org.openrdf.repository.Repository;
-import org.openrdf.repository.SparqlOrderByTest;
-import org.openrdf.repository.sail.SailRepository;
-import org.openrdf.sail.rdbms.postgresql.PgSqlStore;
-
-public class PgSqlSparqlOrderByTest extends SparqlOrderByTest {
-
-	protected Repository newRepository() {
-		PgSqlStore sail = new PgSqlStore("sesame_test");
-		sail.setUser("sesame");
-		sail.setPassword("opensesame");
-		return new SailRepository(sail);
-	}
-}
+// SES-1071 disabling rdbms-based tests
+///*
+// * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+// *
+// * Licensed under the Aduna BSD-style license.
+// */
+//package org.openrdf.repository.sail.rdbms;
+//
+//import org.openrdf.repository.Repository;
+//import org.openrdf.repository.SparqlOrderByTest;
+//import org.openrdf.repository.sail.SailRepository;
+//import org.openrdf.sail.rdbms.postgresql.PgSqlStore;
+//
+//public class PgSqlSparqlOrderByTest extends SparqlOrderByTest {
+//
+//	protected Repository newRepository() {
+//		PgSqlStore sail = new PgSqlStore("sesame_test");
+//		sail.setUser("sesame");
+//		sail.setPassword("opensesame");
+//		return new SailRepository(sail);
+//	}
+//}
--- compliance/store/src/test/java/org/openrdf/sail/rdbms/PgSqlStoreTest.java
+++ compliance/store/src/test/java/org/openrdf/sail/rdbms/PgSqlStoreTest.java
@@ -3,48 +3,49 @@
  *
  * Licensed under the Aduna BSD-style license.
  */
-package org.openrdf.sail.rdbms;
-
-import org.openrdf.sail.RDFStoreTest;
-import org.openrdf.sail.Sail;
-import org.openrdf.sail.SailConnection;
-import org.openrdf.sail.SailException;
-import org.openrdf.sail.rdbms.postgresql.PgSqlStore;
-
-/**
- * An extension of RDFStoreTest for testing the class {@link PgSqlStore}.
- */
-public class PgSqlStoreTest extends RDFStoreTest {
-
-	/*--------------*
-	 * Constructors *
-	 *--------------*/
-
-	public PgSqlStoreTest(String name) {
-		super(name);
-	}
-
-	/*---------*
-	 * Methods *
-	 *---------*/
-
-	@Override
-	protected Sail createSail()
-		throws SailException
-	{
-		PgSqlStore sail = new PgSqlStore("sesame_test");
-		sail.setUser("sesame");
-		sail.setPassword("opensesame");
-		sail.initialize();
-		SailConnection conn = sail.getConnection();
-		try {
-			conn.clear();
-			conn.clearNamespaces();
-			conn.commit();
-		}
-		finally {
-			conn.close();
-		}
-		return sail;
-	}
-}
+// SES-1071 disabling rdbms-based tests
+//package org.openrdf.sail.rdbms;
+//
+//import org.openrdf.sail.RDFStoreTest;
+//import org.openrdf.sail.Sail;
+//import org.openrdf.sail.SailConnection;
+//import org.openrdf.sail.SailException;
+//import org.openrdf.sail.rdbms.postgresql.PgSqlStore;
+//
+///**
+// * An extension of RDFStoreTest for testing the class {@link PgSqlStore}.
+// */
+//public class PgSqlStoreTest extends RDFStoreTest {
+//
+//	/*--------------*
+//	 * Constructors *
+//	 *--------------*/
+//
+//	public PgSqlStoreTest(String name) {
+//		super(name);
+//	}
+//
+//	/*---------*
+//	 * Methods *
+//	 *---------*/
+//
+//	@Override
+//	protected Sail createSail()
+//		throws SailException
+//	{
+//		PgSqlStore sail = new PgSqlStore("sesame_test");
+//		sail.setUser("sesame");
+//		sail.setPassword("opensesame");
+//		sail.initialize();
+//		SailConnection conn = sail.getConnection();
+//		try {
+//			conn.clear();
+//			conn.clearNamespaces();
+//			conn.commit();
+//		}
+//		finally {
+//			conn.close();
+//		}
+//		return sail;
+//	}
+//}
