1b2db9ee92463928db7b972cb94c7a9692d86be4
Jeen Broekstra <jeen.broekstra@gmail.com>
2012-10-16 02:14:25 +0000

    SES-802 reviewed and tested first patch by Peter Ansell for N-Quads support. Made minor modifications to submission only.

---
+++ core/rio/nquads/src/main/java/org/openrdf/rio/nquads/NQuadsParser.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2012.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.rio.nquads;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.ntriples.NTriplesParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * RDF parser implementation for the {@link RDFFormat#NQUADS N-Quads} RDF
+ * format, extending the Rio N-Triples parser. A specification of N-Quads can be
+ * found <a href="http://sw.deri.org/2008/07/n-quads/">here</a>. This parser is
+ * not thread-safe, therefore its public methods are synchronized.
+ * 
+ * @since 2.7.0
+ * @author Joshua Shinavier
+ */
+public class NQuadsParser extends NTriplesParser {
+	protected Resource context;
+
+	@Override
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.NQUADS;
+	}
+
+	@Override
+	public synchronized void parse(final InputStream inputStream, final String baseURI)
+			throws IOException, RDFParseException, RDFHandlerException {
+		if (inputStream == null) {
+			throw new IllegalArgumentException("Input stream can not be 'null'");
+		}
+		// Note: baseURI will be checked in parse(Reader, String)
+
+		try {
+			parse(new InputStreamReader(inputStream, "US-ASCII"), baseURI);
+		} catch (UnsupportedEncodingException e) {
+			// Every platform should support the US-ASCII encoding...
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public synchronized void parse(final Reader reader, final String baseURI)
+			throws IOException, RDFParseException, RDFHandlerException {
+		if (reader == null) {
+			throw new IllegalArgumentException("Reader can not be 'null'");
+		}
+		if (baseURI == null) {
+			throw new IllegalArgumentException("base URI can not be 'null'");
+		}
+
+		rdfHandler.startRDF();
+
+		this.reader = reader;
+		lineNo = 1;
+
+		reportLocation(lineNo, 1);
+
+		try {
+			int c = reader.read();
+			c = skipWhitespace(c);
+
+			while (c != -1) {
+				if (c == '#') {
+					// Comment, ignore
+					c = skipLine(c);
+				} else if (c == '\r' || c == '\n') {
+					// Empty line, ignore
+					c = skipLine(c);
+				} else {
+					c = parseQuad(c);
+				}
+
+				c = skipWhitespace(c);
+			}
+		} finally {
+			clear();
+		}
+
+		rdfHandler.endRDF();
+	}
+
+	private int parseQuad(int c) throws IOException, RDFParseException,
+			RDFHandlerException {
+
+		boolean ignoredAnError = false;
+		try {
+			c = parseSubject(c);
+
+			c = skipWhitespace(c);
+
+			c = parsePredicate(c);
+
+			c = skipWhitespace(c);
+
+			c = parseObject(c);
+
+			c = skipWhitespace(c);
+
+			// Context is not required
+			if (c != '.') {
+				c = parseContext(c);
+				c = skipWhitespace(c);
+			}
+			if (c == -1) {
+				throwEOFException();
+			} else if (c != '.') {
+				reportFatalError("Expected '.', found: " + (char) c);
+			}
+
+			c = assertLineTerminates(c);
+		} catch (RDFParseException rdfpe) {
+			if (stopAtFirstError()) {
+				throw rdfpe;
+			} else {
+				ignoredAnError = true;
+			}
+		}
+
+		c = skipLine(c);
+
+		if (!ignoredAnError) {
+			Statement st = createStatement(subject, predicate, object, context);
+			rdfHandler.handleStatement(st);
+		}
+
+		subject = null;
+		predicate = null;
+		object = null;
+		context = null;
+
+		return c;
+	}
+
+	protected int parseContext(int c) throws IOException, RDFParseException {
+		// FIXME: context (in N-Quads) can be a literal
+		StringBuilder sb = new StringBuilder(100);
+
+		// subject is either an uriref (<foo://bar>) or a nodeID (_:node1)
+		if (c == '<') {
+			// subject is an uriref
+			c = parseUriRef(c, sb);
+			context = createURI(sb.toString());
+		} else if (c == '_') {
+			// subject is a bNode
+			c = parseNodeID(c, sb);
+			context = createBNode(sb.toString());
+		} else if (c == -1) {
+			throwEOFException();
+		} else {
+			reportFatalError("Expected '<' or '_', found: " + (char) c);
+		}
+
+		return c;
+	}
+}
---
+++ core/rio/nquads/src/main/java/org/openrdf/rio/nquads/NQuadsWriterFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2012.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.openrdf.rio.nquads;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.RDFWriterFactory;
+
+/**
+ * An {@link RDFWriterFactory} for {@link RDFFormat#NQUADS N-Quads} writers.
+ * 
+ * @since 2.7.0
+ * @author Peter Ansell
+ */
+public class NQuadsWriterFactory implements RDFWriterFactory {
+
+	/**
+	 * Returns {@link RDFFormat#NQUADS}.
+	 */
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.NQUADS;
+	}
+
+	/**
+	 * Returns a new instance of {@link NQuadsWriter}.
+	 */
+	public RDFWriter getWriter(OutputStream out) {
+		return new NQuadsWriter(out);
+	}
+
+	/**
+	 * Returns a new instance of {@link NQuadsWriter}.
+	 */
+	public RDFWriter getWriter(Writer writer) {
+		return new NQuadsWriter(writer);
+	}
+}
