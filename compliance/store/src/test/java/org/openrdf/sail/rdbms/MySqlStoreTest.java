437cc85a5c371920a3aac5ec3e8fdf3e08a54a18
Jeen Broekstra <jeen.broekstra@gmail.com>
2012-10-31 23:00:39 +0000

    reactivating, as this is really not the correct way to deal with this.

--- compliance/store/src/test/java/org/openrdf/sail/rdbms/MySqlStoreTest.java
+++ compliance/store/src/test/java/org/openrdf/sail/rdbms/MySqlStoreTest.java
@@ -14,37 +14,37 @@ import org.openrdf.sail.rdbms.mysql.MySqlStore;
 /**
  * An extension of RDFStoreTest for testing the class {@link MySqlStore}.
  */
-//public class MySqlStoreTest extends RDFStoreTest {
-//
-//	/*--------------*
-//	 * Constructors *
-//	 *--------------*/
-//
-//	public MySqlStoreTest(String name) {
-//		super(name);
-//	}
-//
-//	/*---------*
-//	 * Methods *
-//	 *---------*/
-//
-//	@Override
-//	protected Sail createSail()
-//		throws SailException
-//	{
-//		MySqlStore sail = new MySqlStore("sesame_test");
-//		sail.setUser("sesame");
-//		sail.setPassword("opensesame");
-//		sail.initialize();
-//		SailConnection conn = sail.getConnection();
-//		try {
-//			conn.clear();
-//			conn.clearNamespaces();
-//			conn.commit();
-//		}
-//		finally {
-//			conn.close();
-//		}
-//		return sail;
-//	}
-//}
+public class MySqlStoreTest extends RDFStoreTest {
+
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
+
+	public MySqlStoreTest(String name) {
+		super(name);
+	}
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	@Override
+	protected Sail createSail()
+		throws SailException
+	{
+		MySqlStore sail = new MySqlStore("sesame_test");
+		sail.setUser("sesame");
+		sail.setPassword("opensesame");
+		sail.initialize();
+		SailConnection conn = sail.getConnection();
+		try {
+			conn.clear();
+			conn.clearNamespaces();
+			conn.commit();
+		}
+		finally {
+			conn.close();
+		}
+		return sail;
+	}
+}
