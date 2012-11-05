829afb65adda85b8993000cc000d4a899a970379
Jeen Broekstra <jeen.broekstra@gmail.com>
2012-11-05 04:35:16 +0000

    SES-1111 fix in initialize: start transaction with begin().

--- core/sail/inferencer/src/main/java/org/openrdf/sail/inferencer/fc/ForwardChainingRDFSInferencer.java
+++ core/sail/inferencer/src/main/java/org/openrdf/sail/inferencer/fc/ForwardChainingRDFSInferencer.java
@@ -60,6 +60,7 @@ public class ForwardChainingRDFSInferencer extends NotifyingSailWrapper {
 
 		ForwardChainingRDFSInferencerConnection con = getConnection();
 		try {
+			con.begin();
 			con.addAxiomStatements();
 			con.commit();
 		}
