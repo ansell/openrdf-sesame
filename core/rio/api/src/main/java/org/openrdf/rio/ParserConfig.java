f4477dc1d1a0a92ef010ddf44f2f112483664f99
Peter Ansell <p_ansell@yahoo.com>
2013-01-07 10:27:09 +1000

    SES-1675 : Convert ParserConfig to use ParserSetting
    All methods should be backwards compatible with previous uses of ParserSetting.

--- core/rio/api/src/main/java/org/openrdf/rio/ParserConfig.java
+++ core/rio/api/src/main/java/org/openrdf/rio/ParserConfig.java
@@ -5,7 +5,14 @@
  */
 package org.openrdf.rio;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.openrdf.rio.RDFParser.DatatypeHandling;
+import org.openrdf.rio.helpers.BasicParserSettings;
 
 /**
  * A container object for easy setting and passing of {@link RDFParser}
@@ -15,13 +22,9 @@ import org.openrdf.rio.RDFParser.DatatypeHandling;
  */
 public class ParserConfig {
 
-	protected boolean verifyData;
+	private final ConcurrentMap<ParserSetting<Object>, Object> settings = new ConcurrentHashMap<ParserSetting<Object>, Object>();
 
-	protected boolean stopAtFirstError;
-
-	protected boolean preserveBNodeIDs;
-
-	protected DatatypeHandling datatypeHandling;
+	protected final Logger log = LoggerFactory.getLogger(ParserConfig.class);
 
 	/**
 	 * Creates a ParserConfig object with default configuration: verifyData is
@@ -29,7 +32,6 @@ public class ParserConfig {
 	 * <tt>false</tt>, and dataTypeHandling is set to VERIFY.
 	 */
 	public ParserConfig() {
-		this(true, true, false, DatatypeHandling.VERIFY);
 	}
 
 	/**
@@ -38,38 +40,99 @@ public class ParserConfig {
 	public ParserConfig(boolean verifyData, boolean stopAtFirstError, boolean preserveBNodeIDs,
 			DatatypeHandling datatypeHandling)
 	{
-		this.verifyData = verifyData;
-		this.stopAtFirstError = stopAtFirstError;
-		this.preserveBNodeIDs = preserveBNodeIDs;
-		this.datatypeHandling = datatypeHandling;
+		this.set(BasicParserSettings.VERIFY_DATA, verifyData);
+		this.set(BasicParserSettings.STOP_AT_FIRST_ERROR, stopAtFirstError);
+		this.set(BasicParserSettings.PRESERVE_BNODE_IDS, preserveBNodeIDs);
+		this.set(BasicParserSettings.DATATYPE_HANDLING, datatypeHandling);
 	}
 
 	/**
-	 * @return Returns the verifyData setting.
+	 * @return Returns the {@link BasicParserSettings#VERIFY_DATA} setting.
 	 */
 	public boolean verifyData() {
-		return verifyData;
+		return this.get(BasicParserSettings.VERIFY_DATA);
 	}
 
 	/**
-	 * @return Returns the stopAtFirstError setting.
+	 * @return Returns the {@link BasicParserSettings#STOP_AT_FIRST_ERROR}
+	 *         setting.
 	 */
 	public boolean stopAtFirstError() {
-		return stopAtFirstError;
+		return this.get(BasicParserSettings.STOP_AT_FIRST_ERROR);
 	}
 
 	/**
-	 * @return Returns the preserveBNodeIDs setting.
+	 * @return Returns the {@link BasicParserSettings#PRESERVE_BNODE_IDS}
+	 *         setting.
 	 */
 	public boolean isPreserveBNodeIDs() {
-		return preserveBNodeIDs;
+		return this.get(BasicParserSettings.PRESERVE_BNODE_IDS);
 	}
 
 	/**
-	 * @return Returns the datatypeHandling setting.
+	 * @return Returns the {@link BasicParserSettings#DATATYPE_HANDLING} setting.
 	 */
 	public DatatypeHandling datatypeHandling() {
-		return datatypeHandling;
+		return this.get(BasicParserSettings.DATATYPE_HANDLING);
 	}
 
+	/**
+	 * Return the value for a given {@link ParserSetting} or the default value if
+	 * it has not been set.
+	 * 
+	 * @param setting
+	 *        The {@link ParserSetting} to fetch a value for.
+	 * @return The value for the parser setting, or the default value if it is
+	 *         not set.
+	 */
+	@SuppressWarnings("unchecked")
+	public final <T extends Object> T get(ParserSetting<T> setting) {
+		Object result = settings.get(setting);
+
+		if (result == null) {
+			return setting.getDefaultValue();
+		}
+
+		return (T)result;
+	}
+
+	/**
+	 * Sets a {@link ParserSetting} to have a new value. If the value is null,
+	 * the parser setting is removed and the default will be used instead.
+	 * 
+	 * @param setting
+	 *        The setting to set a new value for.
+	 * @param value
+	 *        The value for the parser setting, or null to reset the parser
+	 *        setting to use the default value.
+	 */
+	@SuppressWarnings("unchecked")
+	public final <T extends Object> void set(ParserSetting<T> setting, T value) {
+
+		if (value == null) {
+			settings.remove(setting);
+		}
+		else {
+			Object putIfAbsent = settings.putIfAbsent((ParserSetting<Object>)setting, value);
+
+			if (putIfAbsent != null) {
+				settings.put((ParserSetting<Object>)setting, value);
+
+				this.log.trace("Overriding previous setting for {}", setting.getDefaultValue());
+			}
+		}
+	}
+
+	/**
+	 * Checks for whether a {@link ParserSetting} has been explicitly set by a
+	 * user.
+	 * 
+	 * @param setting
+	 *        The setting to check for.
+	 * @return True if the parser setting has been explicitly set, or false
+	 *         otherwise.
+	 */
+	public final <T extends Object> boolean isSet(ParserSetting<T> setting) {
+		return settings.containsKey(setting);
+	}
 }
