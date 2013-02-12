4d23da20682bdc0c9f73f1fc8f3e5c9086bb71f4
Peter Ansell <p_ansell@yahoo.com>
2013-02-12 11:06:30 +1000

    SES-1730 : Override equals and hashCode in NamespaceImpl

--- core/model/src/main/java/org/openrdf/model/impl/NamespaceImpl.java
+++ core/model/src/main/java/org/openrdf/model/impl/NamespaceImpl.java
@@ -109,6 +109,46 @@ public class NamespaceImpl implements Namespace {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Namespace)) {
+			return false;
+		}
+		Namespace other = (Namespace)obj;
+		if (prefix == null) {
+			if (other.getPrefix() != null) {
+				return false;
+			}
+		}
+		else if (!prefix.equals(other.getPrefix())) {
+			return false;
+		}
+		if (name == null) {
+			if (other.getName() != null) {
+				return false;
+			}
+		}
+		else if (!name.equals(other.getName())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((prefix == null) ? 0 : prefix.hashCode());
+		return result;
+	}
+
+	@Override
 	public int compareTo(Namespace o) {
 		if (getPrefix().equals(o.getPrefix())) {
 			if (getName().equals(o.getName())) {
