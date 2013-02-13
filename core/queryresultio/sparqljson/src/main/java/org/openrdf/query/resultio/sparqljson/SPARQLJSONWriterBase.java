e7ae46ed9b98c992c83230d43ca8d10389363029
Peter Ansell <p_ansell@yahoo.com>
2013-02-13 10:51:25 +1000

    SES-1417 : Integrate JSON Writer code into SPARQLJSONWriterBase

--- core/queryresultio/sparqljson/src/main/java/org/openrdf/query/resultio/sparqljson/SPARQLJSONWriterBase.java
+++ core/queryresultio/sparqljson/src/main/java/org/openrdf/query/resultio/sparqljson/SPARQLJSONWriterBase.java
@@ -23,10 +23,19 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.Iterator;
+import java.util.List;
 
 import info.aduna.io.IndentingWriter;
 import info.aduna.text.StringUtil;
 
+import org.openrdf.model.BNode;
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.query.Binding;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryResultHandlerException;
+import org.openrdf.query.TupleQueryResultHandlerException;
 import org.openrdf.query.resultio.QueryResultWriter;
 
 /**
@@ -43,14 +52,253 @@ abstract class SPARQLJSONWriterBase implements QueryResultWriter {
 
 	protected IndentingWriter writer;
 
+	protected boolean firstTupleWritten = false;
+
 	protected boolean documentOpen = false;
 
 	protected boolean headerComplete = false;
+
+	protected boolean tupleVariablesFound = false;
 
 	public SPARQLJSONWriterBase(OutputStream out) {
 		Writer w = new OutputStreamWriter(out, Charset.forName("UTF-8"));
 		w = new BufferedWriter(w, 1024);
 		writer = new IndentingWriter(w);
+	}
+
+	@Override
+	public void endHeader()
+		throws QueryResultHandlerException
+	{
+		try {
+			closeBraces();
+
+			writeComma();
+
+			if (tupleVariablesFound) {
+				// Write results
+				writeKey("results");
+				openBraces();
+
+				writeKey("bindings");
+				openArray();
+			}
+
+			headerComplete = true;
+		}
+		catch (IOException e) {
+			throw new QueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void startQueryResult(List<String> columnHeaders)
+		throws TupleQueryResultHandlerException
+	{
+		try {
+			if (!documentOpen) {
+				startDocument();
+				startHeader();
+			}
+			tupleVariablesFound = true;
+			try {
+				writeKeyValue("vars", columnHeaders);
+			}
+			catch (IOException e) {
+				throw new TupleQueryResultHandlerException(e);
+			}
+		}
+		catch (TupleQueryResultHandlerException e) {
+			throw e;
+		}
+		catch (QueryResultHandlerException e) {
+			throw new TupleQueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void handleSolution(BindingSet bindingSet)
+		throws TupleQueryResultHandlerException
+	{
+		try {
+			if (!headerComplete) {
+				endHeader();
+			}
+			if (firstTupleWritten) {
+				writeComma();
+			}
+			firstTupleWritten = true;
+
+			openBraces(); // start of new solution
+
+			Iterator<Binding> bindingIter = bindingSet.iterator();
+			while (bindingIter.hasNext()) {
+				Binding binding = bindingIter.next();
+
+				writeKeyValue(binding.getName(), binding.getValue());
+
+				if (bindingIter.hasNext()) {
+					writeComma();
+				}
+			}
+
+			closeBraces(); // end solution
+
+			writer.flush();
+		}
+		catch (IOException e) {
+			throw new TupleQueryResultHandlerException(e);
+		}
+		catch (TupleQueryResultHandlerException e) {
+			throw e;
+		}
+		catch (QueryResultHandlerException e) {
+			throw new TupleQueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void endQueryResult()
+		throws TupleQueryResultHandlerException
+	{
+		try {
+			if (!headerComplete) {
+				endHeader();
+			}
+			closeArray(); // bindings array
+			closeBraces(); // results braces
+			endDocument();
+		}
+		catch (IOException e) {
+			throw new TupleQueryResultHandlerException(e);
+		}
+		catch (TupleQueryResultHandlerException e) {
+			throw e;
+		}
+		catch (QueryResultHandlerException e) {
+			throw new TupleQueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void startDocument()
+		throws QueryResultHandlerException
+	{
+		documentOpen = true;
+		headerComplete = false;
+		try {
+			openBraces();
+		}
+		catch (IOException e) {
+			throw new QueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void handleStylesheet(String stylesheetUrl)
+		throws QueryResultHandlerException
+	{
+		// Ignore, as JSON does not support stylesheets
+	}
+
+	@Override
+	public void startHeader()
+		throws QueryResultHandlerException
+	{
+		try {
+			// Write header
+			writeKey("head");
+			openBraces();
+		}
+		catch (IOException e) {
+			throw new QueryResultHandlerException(e);
+		}
+	}
+
+	@Override
+	public void handleLinks(List<String> linkUrls)
+		throws QueryResultHandlerException
+	{
+		try {
+			writeKeyValue("link", linkUrls);
+		}
+		catch (IOException e) {
+			throw new QueryResultHandlerException(e);
+		}
+	}
+
+	protected void writeKeyValue(String key, Value value)
+		throws IOException, QueryResultHandlerException
+	{
+		writeKey(key);
+		writeValue(value);
+	}
+
+	protected void writeValue(Value value)
+		throws IOException, QueryResultHandlerException
+	{
+		writer.write("{ ");
+
+		if (value instanceof URI) {
+			writeKeyValue("type", "uri");
+			writer.write(", ");
+			writeKeyValue("value", ((URI)value).toString());
+		}
+		else if (value instanceof BNode) {
+			writeKeyValue("type", "bnode");
+			writer.write(", ");
+			writeKeyValue("value", ((BNode)value).getID());
+		}
+		else if (value instanceof Literal) {
+			Literal lit = (Literal)value;
+
+			if (lit.getLanguage() != null) {
+				writeKeyValue("xml:lang", lit.getLanguage());
+				writer.write(", ");
+			}
+			if (lit.getDatatype() != null) {
+				writeKeyValue("datatype", lit.getDatatype().toString());
+				writer.write(", ");
+			}
+
+			writeKeyValue("type", "literal");
+
+			writer.write(", ");
+			writeKeyValue("value", lit.getLabel());
+		}
+		else {
+			throw new TupleQueryResultHandlerException("Unknown Value object type: " + value.getClass());
+		}
+
+		writer.write(" }");
+	}
+
+	@Override
+	public void handleBoolean(boolean value)
+		throws QueryResultHandlerException
+	{
+		if (!documentOpen) {
+			startDocument();
+			startHeader();
+		}
+
+		if (!headerComplete) {
+			endHeader();
+		}
+
+		try {
+			if (value) {
+				writeKeyValue("boolean", "true");
+			}
+			else {
+				writeKeyValue("boolean", "false");
+			}
+
+			endDocument();
+		}
+		catch (IOException e) {
+			throw new QueryResultHandlerException(e);
+		}
 	}
 
 	protected void endDocument()
