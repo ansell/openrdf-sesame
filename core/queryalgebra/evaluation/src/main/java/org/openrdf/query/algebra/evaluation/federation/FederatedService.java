68f34e7016bf03cd698e821b2eae649a659bee4f
Dale Visser <dvisser@ida.org>
2013-03-19 14:54:53 -0400

    Fixes for javadoc warnings in queryalgebra-federation, as well as some Eclipse automatic code re-formatting.

--- core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/federation/FederatedService.java
+++ core/queryalgebra/evaluation/src/main/java/org/openrdf/query/algebra/evaluation/federation/FederatedService.java
@@ -25,11 +25,10 @@ import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sparql.query.InsertBindingSetCursor;
 
 /**
- * FederatedService to allow for customized evaluation of SERVICE expression.
- * By default {@link SPARQLFederatedService} is used.
+ * FederatedService to allow for customized evaluation of SERVICE expression. By
+ * default {@link SPARQLFederatedService} is used.
  * 
  * @author Andreas Schwarte
- * 
  * @see SPARQLFederatedService
  */
 public interface FederatedService {
@@ -37,14 +36,19 @@ public interface FederatedService {
 	/**
 	 * Query type
 	 */
-	public static enum QueryType { SELECT, ASK }
-	
-	
+	public static enum QueryType {
+		SELECT,
+		ASK
+	}
+
 	/**
-	 * Evaluate the provided SPARQL query at this federated service.<p>
-	 * 
-	 * <b>Important:</b> The original bindings need to be inserted into the result, e.g.
-	 * via {@link InsertBindingSetCursor}.<p>
+	 * <p>
+	 * Evaluate the provided SPARQL query at this federated service.
+	 * </p>
+	 * <p>
+	 * <b>Important:</b> The original bindings need to be inserted into the
+	 * result, e.g. via {@link InsertBindingSetCursor}.
+	 * </p>
 	 * 
 	 * <pre>
 	 * Expected behavior:
@@ -55,61 +59,61 @@ public interface FederatedService {
 	 * </pre>
 	 * 
 	 * @param sparqlQueryString
-	 * 				a SPARQL query (either SELECT or ASK, compare type parameter)
+	 *        a SPARQL query (either SELECT or ASK, compare type parameter)
 	 * @param bindings
-	 * 				the bindings serving as additional constraints
+	 *        the bindings serving as additional constraints
 	 * @param baseUri
 	 * @param type
-	 * 				the {@link QueryType}, either ASK or SELECT
+	 *        the {@link QueryType}, either ASK or SELECT
 	 * @param service
-	 * 				the reference to the service node, contains additional meta information (vars, prefixes)
-	 * 
-	 * @return
+	 *        the reference to the service node, contains additional meta
+	 *        information (vars, prefixes)
+	 * @return an iteration over the results of the query
 	 * @throws QueryEvaluationException
 	 */
-	public CloseableIteration<BindingSet, QueryEvaluationException> evaluate(
-			String sparqlQueryString, BindingSet bindings, String baseUri,
-			QueryType type, Service service) throws QueryEvaluationException;
-	
-	
+	public CloseableIteration<BindingSet, QueryEvaluationException> evaluate(String sparqlQueryString,
+			BindingSet bindings, String baseUri, QueryType type, Service service)
+		throws QueryEvaluationException;
+
 	/**
-	 * Evaluate the provided SPARQL query at this federated service, possibilities for
-	 * vectored evaluation.<p>
-	 * 
-	 * <b>Contracts:</b> 
+	 * Evaluate the provided SPARQL query at this federated service,
+	 * possibilities for vectored evaluation.
+	 * <p>
+	 * <b>Contracts:</b>
 	 * <ul>
-	 * 	<li>The original bindings need to be inserted into the result, e.g.
-	 *		 via {@link InsertBindingSetCursor}.</li>
-	 *  <li>SILENT service must be dealt with in the method</li>
+	 * <li>The original bindings need to be inserted into the result, e.g. via
+	 * {@link InsertBindingSetCursor}.</li>
+	 * <li>SILENT service must be dealt with in the method</li>
 	 * </ul>
+	 * <p>
+	 * Compare {@link SPARQLFederatedService} for a reference implementation
+	 * </p>
 	 * 
-	 * <p>Compare {@link SPARQLFederatedService} for a reference implementation</p>
-	 *  
 	 * @param service
-	 * 				the reference to the service node, contains information to construct the query
+	 *        the reference to the service node, contains information to
+	 *        construct the query
 	 * @param bindings
-	 * 				the bindings serving as additional constraints (for vectored evaluation)
+	 *        the bindings serving as additional constraints (for vectored
+	 *        evaluation)
 	 * @param baseUri
-	 * 				the baseUri
-	 * 
-	 * @return
-	 * 			the result of evaluating the query using bindings as constraints, the original 
-	 * 			bindings need to be inserted into the results!
-	 * 
+	 *        the baseUri
+	 * @return the result of evaluating the query using bindings as constraints,
+	 *         the original bindings need to be inserted into the results!
 	 * @throws QueryEvaluationException
 	 */
-	public CloseableIteration<BindingSet, QueryEvaluationException> evaluate(
-			Service service,
-			CloseableIteration<BindingSet, QueryEvaluationException> bindings,
-			String baseUri) throws QueryEvaluationException;
-	
+	public CloseableIteration<BindingSet, QueryEvaluationException> evaluate(Service service,
+			CloseableIteration<BindingSet, QueryEvaluationException> bindings, String baseUri)
+		throws QueryEvaluationException;
+
 	/**
 	 * Method to perform any initializations, invoked after construction.
 	 */
-	public void initialize() throws RepositoryException;
-	
+	public void initialize()
+		throws RepositoryException;
+
 	/**
 	 * Method to perform any shutDown code, invoked at unregistering.
 	 */
-	public void shutdown() throws RepositoryException;
+	public void shutdown()
+		throws RepositoryException;
 }
