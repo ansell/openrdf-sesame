af3a00f846da24ef81a5f77e7d32a6521e31a535
Dale Visser <dvisser@ida.org>
2013-03-25 10:38:33 -0400

    SES-1780 Added regression test.

---
+++ core/http/workbench/src/test/java/org/openrdf/workbench/util/TestTupleResultBuilder.java
@@ -0,0 +1,64 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.workbench.util;
+
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.query.QueryResultHandlerException;
+import org.openrdf.query.resultio.sparqljson.SPARQLResultsJSONWriter;
+
+/**
+ * @author Dale Visser
+ */
+public class TestTupleResultBuilder {
+
+	private TupleResultBuilder builder;
+
+	@Before
+	public void setUp() {
+		builder = new TupleResultBuilder(new SPARQLResultsJSONWriter(new ByteArrayOutputStream()),
+				ValueFactoryImpl.getInstance());
+	}
+
+	@After
+	public void tearDown()
+		throws QueryResultHandlerException
+	{
+		builder.end();
+	}
+
+	@Test
+	public final void testSES1780regression()
+		throws MalformedURLException
+	{
+		try {
+			builder.namedResult("test", new URL("http://www.foo.org/bar#"));
+		}
+		catch (QueryResultHandlerException e) {
+			fail(e.getMessage());
+		}
+	}
+}
