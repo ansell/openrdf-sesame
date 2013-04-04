558278c861c915ac6f23b0b3e4ad1caf460f9321
Peter Ansell <p_ansell@yahoo.com>
2013-04-04 15:32:03 +1000

    SES-1784 : Add RDF/JSON parser and writer and factories

---
+++ core/rio/rdfjson/src/main/java/org/openrdf/rio/rdfjson/RDFJSONWriterFactory.java
@@ -0,0 +1,48 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.rio.rdfjson;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.RDFWriterFactory;
+
+/**
+ * An {@link RDFWriterFactory} for RDF/JSON writers.
+ * 
+ * @author Peter Ansell
+ */
+public class RDFJSONWriterFactory implements RDFWriterFactory {
+
+	@Override
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.RDFJSON;
+	}
+
+	@Override
+	public RDFWriter getWriter(final OutputStream out) {
+		return new RDFJSONWriter(out, this.getRDFFormat());
+	}
+
+	@Override
+	public RDFWriter getWriter(final Writer writer) {
+		return new RDFJSONWriter(writer, this.getRDFFormat());
+	}
+
+}
