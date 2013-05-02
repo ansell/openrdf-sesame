d4a1ff7deb4830b80a8552782fa8228d68a5b3e7
Peter Ansell <p_ansell@yahoo.com>
2013-05-02 14:30:00 +1000

    SES-1819 : Add first test for MemTripleSource

---
+++ core/sail/memory/src/test/java/org/openrdf/sail/memory/MemTripleSourceTest.java
@@ -0,0 +1,130 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.sail.memory;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import info.aduna.iteration.CloseableIteration;
+import info.aduna.iteration.Iterations;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.Rio;
+import org.openrdf.sail.SailException;
+import org.openrdf.sail.memory.MemoryStoreConnection.MemTripleSource;
+import org.openrdf.sail.memory.model.MemStatement;
+import org.openrdf.sail.memory.model.ReadMode;
+
+/**
+ * Unit Test for {@link MemTripleSource}
+ * 
+ * @author Peter Ansell
+ */
+public class MemTripleSourceTest {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private MemoryStore store;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp()
+		throws Exception
+	{
+		store = new MemoryStore();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown()
+		throws Exception
+	{
+		store.shutDown();
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.MemoryStoreConnection.MemTripleSource#getStatements(org.openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value, org.openrdf.model.Resource[])}
+	 * .
+	 */
+	@Test
+	public final void testGetStatementsNoContextsAllNull()
+		throws Exception
+	{
+		loadTestData("/alp-testdata.ttl");
+		MemTripleSource source = getTripleSourceCommitted();
+
+		CloseableIteration<MemStatement, QueryEvaluationException> statements = source.getStatements(null,
+				null, null);
+
+		try {
+			List<MemStatement> list = Iterations.asList(statements);
+
+			assertEquals(8, list.size());
+		}
+		finally {
+			statements.close();
+		}
+	}
+
+	protected void loadTestData(String dataFile, Resource... contexts)
+		throws RDFParseException, IOException, SailException
+	{
+		logger.debug("loading dataset {}", dataFile);
+		InputStream dataset = this.getClass().getResourceAsStream(dataFile);
+		try {
+			store.startTransaction();
+			for (Statement nextStatement : Rio.parse(dataset, "", RDFFormat.TURTLE, contexts)) {
+				store.addStatement(nextStatement.getSubject(), nextStatement.getPredicate(),
+						nextStatement.getObject(), nextStatement.getContext(), true);
+			}
+		}
+		finally {
+			store.commit();
+			dataset.close();
+		}
+		logger.debug("dataset loaded.");
+	}
+
+	/**
+	 * Helper method to avoid writing this constructor multiple times. It needs
+	 * to be created after statements are added and committed.
+	 * 
+	 * @return
+	 */
+	private MemTripleSource getTripleSourceCommitted() {
+		return new MemTripleSource(store, true, store.getCurrentSnapshot(), ReadMode.COMMITTED);
+	}
+
+}
