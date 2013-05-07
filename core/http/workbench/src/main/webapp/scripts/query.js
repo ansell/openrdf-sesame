2aea8570d1472395b588a1b76dbcfde132c93569
Dale Visser <dvisser@ida.org>
2013-05-07 15:05:06 -0400

    SES-1799 Fixed form initialization bug, and values submitted to server are now correct.
    jQuery form.serialize() sends the text of the value attribute for checkboxes, only if checked.
    If not checked, nothing is sent.

--- core/http/workbench/src/main/webapp/scripts/query.js
+++ core/http/workbench/src/main/webapp/scripts/query.js
@@ -186,9 +186,7 @@ function trimQuery() {
  * 'save privately' option.
  */
 function disablePrivateSaveForAnonymous() {
-	var user = document.getElementById('selected-user').getElementsByTagName(
-			'span')[0].innerHTML;
-	if (user == 'None') {
+	if ($('td#selected-user>span.disabled').length == 1) {
 		var checkbox = document.getElementById('save-private');
 		checkbox.setAttribute('value', false);
 		checkbox.setAttribute('disabled', 'disabled');
