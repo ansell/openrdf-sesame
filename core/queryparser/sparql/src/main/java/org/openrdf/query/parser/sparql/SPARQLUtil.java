c4dbf309651a2c109d022d0ee4c77ee1e3799108
Peter Ansell <p_ansell@yahoo.com>
2013-05-27 14:59:16 +1000

    SES-1803 : Replace SPARQLUtil.decodeString with NTriplesUtil.unescapeString

--- core/queryparser/sparql/src/main/java/org/openrdf/query/parser/sparql/SPARQLUtil.java
+++ core/queryparser/sparql/src/main/java/org/openrdf/query/parser/sparql/SPARQLUtil.java
@@ -16,7 +16,7 @@
  */
 package org.openrdf.query.parser.sparql;
 
-import info.aduna.text.StringUtil;
+import org.openrdf.rio.ntriples.NTriplesUtil;
 
 /**
  * SPARQL-related utility methods.
@@ -30,15 +30,7 @@ public class SPARQLUtil {
 	 * query.
 	 */
 	public static String encodeString(String s) {
-		s = StringUtil.gsub("\\", "\\\\", s);
-		s = StringUtil.gsub("\t", "\\t", s);
-		s = StringUtil.gsub("\n", "\\n", s);
-		s = StringUtil.gsub("\r", "\\r", s);
-		s = StringUtil.gsub("\b", "\\b", s);
-		s = StringUtil.gsub("\f", "\\f", s);
-		s = StringUtil.gsub("\"", "\\\"", s);
-		s = StringUtil.gsub("'", "\\'", s);
-		return s;
+		return NTriplesUtil.escapeString(s);
 	}
 
 	/**
@@ -53,67 +45,6 @@ public class SPARQLUtil {
 	 *            string.
 	 */
 	public static String decodeString(String s) {
-		int backSlashIdx = s.indexOf('\\');
-
-		if (backSlashIdx == -1) {
-			// No escaped characters found
-			return s;
-		}
-
-		int startIdx = 0;
-		int sLength = s.length();
-		StringBuilder sb = new StringBuilder(sLength);
-
-		while (backSlashIdx != -1) {
-			sb.append(s.substring(startIdx, backSlashIdx));
-
-			if (backSlashIdx + 1 >= sLength) {
-				throw new IllegalArgumentException("Unescaped backslash in: " + s);
-			}
-
-			char c = s.charAt(backSlashIdx + 1);
-
-			if (c == 't') {
-				sb.append('\t');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == 'n') {
-				sb.append('\n');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == 'r') {
-				sb.append('\r');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == 'b') {
-				sb.append('\b');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == 'f') {
-				sb.append('\f');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == '"') {
-				sb.append('"');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == '\'') {
-				sb.append('\'');
-				startIdx = backSlashIdx + 2;
-			}
-			else if (c == '\\') {
-				sb.append('\\');
-				startIdx = backSlashIdx + 2;
-			}
-			else {
-				throw new IllegalArgumentException("Unescaped backslash in: " + s);
-			}
-
-			backSlashIdx = s.indexOf('\\', startIdx);
-		}
-
-		sb.append(s.substring(startIdx));
-
-		return sb.toString();
+		return NTriplesUtil.unescapeString(s);
 	}
 }
