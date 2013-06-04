cb06589c8553484a7bf9cdbeb6ecd309faceae44
Jeen Broekstra <jeen.broekstra@gmail.com>
2013-06-04 18:24:15 +1200

    SES-1851 added a few tests at protocol level to check correct content type is set for various kinds of GET queries.

--- compliance/http/src/test/java/org/openrdf/http/server/ProtocolTest.java
+++ compliance/http/src/test/java/org/openrdf/http/server/ProtocolTest.java
@@ -22,17 +22,23 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 import info.aduna.io.IOUtil;
 
 import org.openrdf.http.protocol.Protocol;
 import org.openrdf.model.impl.URIImpl;
+import org.openrdf.query.GraphQueryResult;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.impl.GraphQueryResultImpl;
 import org.openrdf.query.resultio.QueryResultIO;
 import org.openrdf.query.resultio.TupleQueryResultFormat;
 import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
 
 public class ProtocolTest extends TestCase {
 
@@ -128,9 +134,136 @@ public class ProtocolTest extends TestCase {
 	public void testSeRQLselect()
 		throws Exception
 	{
-		TupleQueryResult queryResult = evaluate(TestServer.REPOSITORY_URL, "select * from {X} P {Y}",
+		TupleQueryResult queryResult = evaluateTupleQuery(TestServer.REPOSITORY_URL, "select * from {X} P {Y}",
 				QueryLanguage.SERQL);
 		QueryResultIO.write(queryResult, TupleQueryResultFormat.SPARQL, System.out);
+	}
+	
+
+	/**
+	 * Checks that the requested content type is returned when accept header explicitly set.
+	 */
+	@Test
+	public void testContentTypeForGraphQuery1_GET()
+		throws Exception
+	{
+		String query = "DESCRIBE <foo:bar>";
+		String location = TestServer.REPOSITORY_URL;
+		location += "?query=" + URLEncoder.encode(query, "UTF-8");
+
+		URL url = new URL(location);
+
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+		// Request RDF/XML formatted results:
+		conn.setRequestProperty("Accept", RDFFormat.RDFXML.getDefaultMIMEType());
+
+		conn.connect();
+
+		try {
+			int responseCode = conn.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				String contentType = conn.getHeaderField("Content-Type");
+				assertNotNull(contentType);
+				
+				// snip off optional charset declaration
+				int charPos = contentType.indexOf(";");
+				if (charPos > -1) {
+					contentType = contentType.substring(0, charPos);
+				}
+				
+				assertEquals(RDFFormat.RDFXML.getDefaultMIMEType(), contentType);
+			}
+			else {
+				String response = "location " + location + " responded: " + conn.getResponseMessage() + " ("
+						+ responseCode + ")";
+				fail(response);
+				throw new RuntimeException(response);
+			}
+		}
+		finally {
+			conn.disconnect();
+		}
+	}
+	
+	/**
+	 * Checks that a proper error (HTTP 406) is returned when accept header is set incorrectly on graph query.
+	 */
+	@Test
+	public void testContentTypeForGraphQuery2_GET()
+		throws Exception
+	{
+		String query = "DESCRIBE <foo:bar>";
+		String location = TestServer.REPOSITORY_URL;
+		location += "?query=" + URLEncoder.encode(query, "UTF-8");
+
+		URL url = new URL(location);
+
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+		// incorrect mime-type for graph query results
+		conn.setRequestProperty("Accept", TupleQueryResultFormat.SPARQL.getDefaultMIMEType());
+		
+		conn.connect();
+
+		try {
+			int responseCode = conn.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_NOT_ACCEPTABLE) {
+			}
+			else {
+				String response = "location " + location + " responded: " + conn.getResponseMessage() + " ("
+						+ responseCode + ")";
+				fail(response);
+			}
+		}
+		finally {
+			conn.disconnect();
+		}
+	}
+	
+
+	/**
+	 * Checks that a suitable RDF content type is returned when accept header not explicitly set.
+	 */
+	@Test
+	public void testContentTypeForGraphQuery3_GET()
+		throws Exception
+	{
+		String query = "DESCRIBE <foo:bar>";
+		String location = TestServer.REPOSITORY_URL;
+		location += "?query=" + URLEncoder.encode(query, "UTF-8");
+
+		URL url = new URL(location);
+
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+		conn.connect();
+
+		try {
+			int responseCode = conn.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				String contentType = conn.getHeaderField("Content-Type");
+				assertNotNull(contentType);
+				
+				// snip off optional charset declaration
+				int charPos = contentType.indexOf(";");
+				if (charPos > -1) {
+					contentType = contentType.substring(0, charPos);
+				}
+				
+				RDFFormat mimeType = RDFFormat.forMIMEType(contentType);
+				assertNotNull(mimeType);
+			}
+			else {
+				String response = "location " + location + " responded: " + conn.getResponseMessage() + " ("
+						+ responseCode + ")";
+				fail(response);
+				throw new RuntimeException(response);
+			}
+		}
+		finally {
+			conn.disconnect();
+		}
 	}
 
 	private void putFile(String location, String file)
@@ -194,7 +327,7 @@ public class ProtocolTest extends TestCase {
 		}
 	}
 
-	private TupleQueryResult evaluate(String location, String query, QueryLanguage queryLn)
+	private TupleQueryResult evaluateTupleQuery(String location, String query, QueryLanguage queryLn)
 		throws Exception
 	{
 		location += "?query=" + URLEncoder.encode(query, "UTF-8") + "&queryLn=" + queryLn.getName();
@@ -225,4 +358,5 @@ public class ProtocolTest extends TestCase {
 			conn.disconnect();
 		}
 	}
+	
 }
