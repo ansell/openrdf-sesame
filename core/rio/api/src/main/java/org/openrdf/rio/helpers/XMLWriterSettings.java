4d25ed9e6213b273ba0ace15d244377c5eb60daf
Peter Ansell <p_ansell@yahoo.com>
2013-06-21 12:47:42 +1000

    SES-1866 : Add XMLWriterSettings
    Initial setting is to allow users not to print the XML PI, for cases
    where the stream already has the XML PI and possibly other information
    already printed to it

---
+++ core/rio/api/src/main/java/org/openrdf/rio/helpers/XMLWriterSettings.java
@@ -0,0 +1,47 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.rio.helpers;
+
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.RioSetting;
+
+/**
+ * A class encapsulating writer settings that XML writers may support.
+ * 
+ * @author Peter Ansell
+ * @since 2.7.3
+ */
+public class XMLWriterSettings {
+
+	/**
+	 * Boolean setting for XML Writer to determine whether the XML PI (Processing
+	 * Instruction) should be printed. If this setting is disabled the user must
+	 * have previously printed the XML PI before calling
+	 * {@link RDFWriter#startRDF()} for the document to be valid XML.
+	 * <p>
+	 * Defaults to true.
+	 */
+	public static final RioSetting<Boolean> INCLUDE_XML_PI = new RioSettingImpl<Boolean>(
+			"org.openrdf.rio.includexmlpi", "Include XML Processing Instruction", Boolean.TRUE);
+
+	/**
+	 * Private default constructor.
+	 */
+	private XMLWriterSettings() {
+	}
+
+}
