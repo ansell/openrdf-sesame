99edf6526ab464929709a07fa5f80db382078958
Peter Ansell <p_ansell@yahoo.com>
2013-09-16 11:29:41 +1000

    SES-1933 : Add unit test for QueryEvaluationUtil.compare

--- core/queryalgebra/evaluation/src/test/java/org/openrdf/query/algebra/evaluation/util/QueryEvaluationUtilTest.java
+++ core/queryalgebra/evaluation/src/test/java/org/openrdf/query/algebra/evaluation/util/QueryEvaluationUtilTest.java
@@ -16,75 +16,196 @@
  */
 package org.openrdf.query.algebra.evaluation.util;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.openrdf.query.algebra.Compare.CompareOp.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import org.openrdf.model.Literal;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.model.vocabulary.XMLSchema;
-
+import org.openrdf.query.algebra.Compare.CompareOp;
+import org.openrdf.query.algebra.evaluation.ValueExprEvaluationException;
 
 /**
- *
  * @author Jeen Broekstra
  */
 public class QueryEvaluationUtilTest {
 
 	private ValueFactory f = ValueFactoryImpl.getInstance();
 	
+	private Literal arg1simple;
+	private Literal arg2simple;
+	private Literal arg1en;
+	private Literal arg2en;
+	private Literal arg1cy;
+	private Literal arg2cy;
+	private Literal arg1string;
+	private Literal arg2string;
+	private Literal arg1int;
+	private Literal arg2int;
+	private Literal arg1year;
+	private Literal arg2year;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		arg1simple = f.createLiteral("abc");
+		arg2simple = f.createLiteral("b");
+
+		arg1en = f.createLiteral("abc", "en");
+		arg2en = f.createLiteral("b", "en");
+
+		arg1cy = f.createLiteral("abc", "cy");
+		arg2cy = f.createLiteral("b", "cy");
+
+		arg1string = f.createLiteral("abc", XMLSchema.STRING);
+		arg2string = f.createLiteral("b", XMLSchema.STRING);
+
+		arg1year = f.createLiteral("2007", XMLSchema.GYEAR);
+		arg2year = f.createLiteral("2009", XMLSchema.GYEAR);
+
+		arg1int = f.createLiteral(10);
+		arg2int = f.createLiteral(1);
+	}
+	
 	@Test
-	public void testCompatibleArguments() throws Exception {
-		
-		Literal arg1simple = f.createLiteral("abc");
-		Literal arg2simple = f.createLiteral("b");
-		
-		Literal arg1en = f.createLiteral("abc", "en");
-		Literal arg2en = f.createLiteral("b", "en");
-		
-		Literal arg1cy = f.createLiteral("abc", "cy");
-		Literal arg2cy = f.createLiteral("b", "cy");
-		
-		Literal arg1string = f.createLiteral("abc", XMLSchema.STRING);
-		Literal arg2string = f.createLiteral("b", XMLSchema.STRING);
-		
-		Literal arg1int = f.createLiteral(10);
-		Literal arg2int = f.createLiteral(1);
-		
+	public void testCompatibleArguments()
+		throws Exception
+	{
+
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1simple, arg2simple));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1simple, arg2en));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1simple, arg2cy));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1simple, arg2string));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1simple, arg2int));
-		
+
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1en, arg2simple));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1en, arg2en));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg2en, arg2cy));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1en, arg2cy));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1en, arg2string));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1en, arg2int));
-		
+
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1cy, arg2simple));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1cy, arg2en));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg2cy, arg2en));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1cy, arg2cy));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1cy, arg2string));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1cy, arg2int));
-		
+
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1string, arg2simple));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1string, arg2en));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1string, arg2cy));
 		assertTrue(QueryEvaluationUtil.compatibleArguments(arg1string, arg2string));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1string, arg2int));
-		
+
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1int, arg2simple));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1int, arg2en));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1int, arg2cy));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1int, arg2string));
 		assertFalse(QueryEvaluationUtil.compatibleArguments(arg1int, arg2int));
-		
+
 	}
-	
+
+	@Test
+	public void testCompare()
+		throws Exception
+	{
+		assertCompareFalse(arg1simple, arg2simple, EQ);
+		assertCompareFalse(arg1simple, arg2en, EQ);
+		assertCompareFalse(arg1simple, arg2cy, EQ);
+		assertCompareFalse(arg1simple, arg2string, EQ);
+		assertCompareException(arg1simple, arg2int, EQ);
+		assertCompareException(arg1simple, arg2year, EQ);
+
+		assertCompareFalse(arg1en, arg2simple, EQ);
+		assertCompareFalse(arg1en, arg2en, EQ);
+		assertCompareFalse(arg2en, arg2cy, EQ);
+		assertCompareFalse(arg1en, arg2cy, EQ);
+		assertCompareFalse(arg1en, arg2string, EQ);
+		assertCompareFalse(arg1en, arg2int, EQ);
+
+		assertCompareFalse(arg1cy, arg2simple, EQ);
+		assertCompareFalse(arg1cy, arg2en, EQ);
+		assertCompareFalse(arg2cy, arg2en, EQ);
+		assertCompareFalse(arg1cy, arg2cy, EQ);
+		assertCompareFalse(arg1cy, arg2string, EQ);
+		assertCompareFalse(arg1cy, arg2int, EQ);
+
+		assertCompareFalse(arg1string, arg2simple, EQ);
+		assertCompareFalse(arg1string, arg2en, EQ);
+		assertCompareFalse(arg1string, arg2cy, EQ);
+		assertCompareFalse(arg1string, arg2string, EQ);
+		assertCompareFalse(arg1string, arg2int, EQ);
+		assertCompareException(arg1string, arg2year, EQ);
+
+		assertCompareException(arg1int, arg2simple, EQ);
+		assertCompareFalse(arg1int, arg2en, EQ);
+		assertCompareFalse(arg1int, arg2cy, EQ);
+		assertCompareFalse(arg1int, arg2string, EQ);
+		assertCompareFalse(arg1int, arg2int, EQ);
+		assertCompareException(arg1int, arg2year, EQ);
+
+	}
+
+	/**
+	 * Assert that there is an exception as a result of comparing the two
+	 * literals with the given operator.
+	 * 
+	 * @param lit1
+	 *        The left literal
+	 * @param lit2
+	 *        The right literal
+	 * @param op
+	 *        The operator for the comparison
+	 */
+	private void assertCompareException(Literal lit1, Literal lit2, CompareOp op)
+		throws Exception
+	{
+		try {
+			QueryEvaluationUtil.compareLiterals(lit1, lit2, op);
+			fail("Did not receive expected ValueExprEvaluationException");
+		}
+		catch (ValueExprEvaluationException e) {
+			// Expected exception
+		}
+	}
+
+	/**
+	 * Assert that there is no exception as a result of comparing the two
+	 * literals with the given operator and it returns false.
+	 * 
+	 * @param lit1
+	 *        The left literal
+	 * @param lit2
+	 *        The right literal
+	 * @param op
+	 *        The operator for the comparison
+	 */
+	private void assertCompareFalse(Literal lit1, Literal lit2, CompareOp op)
+		throws Exception
+	{
+		assertFalse(QueryEvaluationUtil.compareLiterals(lit1, lit2, op));
+	}
+
+	/**
+	 * Assert that there is no exception as a result of comparing the two
+	 * literals with the given operator and it returns true.
+	 * 
+	 * @param lit1
+	 *        The left literal
+	 * @param lit2
+	 *        The right literal
+	 * @param op
+	 *        The operator for the comparison
+	 */
+	private void assertCompareTrue(Literal lit1, Literal lit2, CompareOp op)
+		throws Exception
+	{
+		assertTrue(QueryEvaluationUtil.compareLiterals(lit1, lit2, op));
+	}
+
 }
