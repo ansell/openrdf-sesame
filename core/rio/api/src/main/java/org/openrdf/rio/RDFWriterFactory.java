cf1de4beb044fefa193c203b2e003cbb58d920ca
Peter Ansell <p_ansell@yahoo.com>
2013-09-23 15:01:23 +1000

    SES-1871 : Javadoc changes to RDFWriterFactory

--- core/rio/api/src/main/java/org/openrdf/rio/RDFWriterFactory.java
+++ core/rio/api/src/main/java/org/openrdf/rio/RDFWriterFactory.java
@@ -42,10 +42,15 @@ public interface RDFWriterFactory {
 
 	/**
 	 * Returns an RDFWriter instance that will write to the supplied output
-	 * stream.
+	 * stream, using the specified URI for the overall document base URI.
 	 * 
 	 * @param out
 	 *        The OutputStream to write the RDF to.
+	 * @param defaultBaseURI
+	 *        An optional base URI to use as the default base URI for the
+	 *        resulting RDFWriter. If null, then the default, if any, base URI,
+	 *        for the given format must be used.
+	 * @since 2.8.0
 	 */
 	public RDFWriter getWriter(OutputStream out, String defaultBaseURI);
 
@@ -62,14 +67,20 @@ public interface RDFWriterFactory {
 	public RDFWriter getWriter(Writer writer);
 
 	/**
-	 * Returns an RDFWriter instance that will write to the supplied writer.
-	 * (Optional operation)
+	 * Returns an RDFWriter instance that will write to the supplied writer,
+	 * using the specified URI for the overall document base URI. (Optional
+	 * operation)
 	 * 
 	 * @param writer
 	 *        The Writer to write the RDF to.
+	 * @param defaultBaseURI
+	 *        An optional base URI to use as the default base URI for the
+	 *        resulting RDFWriter. If null, then the default, if any, base URI,
+	 *        for the given format must be used.
 	 * @throws UnsupportedOperationException
 	 *         if the RDFWriter the specific format does not support writing to a
 	 *         {@link java.io.Writer}
+	 * @since 2.8.0
 	 */
 	public RDFWriter getWriter(Writer writer, String defaultBaseURI);
 }
