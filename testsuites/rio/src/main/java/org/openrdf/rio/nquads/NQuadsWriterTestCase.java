c1acd3b97d06edf7accd2bf47a048aa4fa139734
Peter Ansell <p_ansell@yahoo.com>
2013-10-31 12:53:33 +1100

    SES-1945 : Fix NQuads test that regressed when the default changed

--- testsuites/rio/src/main/java/org/openrdf/rio/nquads/NQuadsWriterTestCase.java
+++ testsuites/rio/src/main/java/org/openrdf/rio/nquads/NQuadsWriterTestCase.java
@@ -43,35 +43,36 @@ import org.openrdf.rio.RDFParserFactory;
 import org.openrdf.rio.RDFWriter;
 import org.openrdf.rio.RDFWriterFactory;
 import org.openrdf.rio.RDFWriterTest;
+import org.openrdf.rio.helpers.BasicWriterSettings;
 import org.openrdf.rio.helpers.StatementCollector;
 import org.openrdf.sail.memory.MemoryStore;
 
 public abstract class NQuadsWriterTestCase extends RDFWriterTest {
 
 	private RDFParser parser;
+
 	private RDFWriter writer;
-   private ValueFactory vf;
-	
+
+	private ValueFactory vf;
+
 	protected NQuadsWriterTestCase(RDFWriterFactory writerF, RDFParserFactory parserF) {
 		super(writerF, parserF);
 	}
 
 	@Before
-	public void setUp() 
-			throws Exception 
+	public void setUp()
+		throws Exception
 	{
-      parser = rdfParserFactory.getParser();
-      vf = ValueFactoryImpl.getInstance();
+		parser = rdfParserFactory.getParser();
+		vf = ValueFactoryImpl.getInstance();
 	}
-	
-   @After
-   public void tearDown() 
-   {
-   	 parser = null;
-       writer = null;
-   }
-	
-	
+
+	@After
+	public void tearDown() {
+		parser = null;
+		writer = null;
+	}
+
 	public void testWrite()
 		throws RepositoryException, RDFParseException, IOException, RDFHandlerException
 	{
@@ -104,10 +105,10 @@ public abstract class NQuadsWriterTestCase extends RDFWriterTest {
 				RepositoryUtil.equals(rep1, rep2));
 	}
 
-   @Test
-   public void testReadWrite() 
-   		throws RDFHandlerException, IOException, RDFParseException 
-   {
+	@Test
+	public void testReadWrite()
+		throws RDFHandlerException, IOException, RDFParseException
+	{
 		StatementCollector statementCollector = new StatementCollector();
 		parser.setRDFHandler(statementCollector);
 		parser.parse(this.getClass().getClassLoader().getResourceAsStream("testcases/nquads/test2.nq"),
@@ -124,41 +125,89 @@ public abstract class NQuadsWriterTestCase extends RDFWriterTest {
 		writer.endRDF();
 
 		Assert.assertEquals("Unexpected number of lines.", 400, baos.toString().split("\n").length);
-   }
-   
-   @Test
-   public void testNoContext() 
-   		throws RDFHandlerException 
-   {
-       Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"), vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"));
-       
-       ByteArrayOutputStream baos = new ByteArrayOutputStream();
-       writer = rdfWriterFactory.getWriter(baos);
-       writer.startRDF();
-       writer.handleStatement(s1);
-       writer.endRDF();
-       
-       String content = baos.toString();
-       String[] lines = content.split("\n");
-       Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
-       Assert.assertEquals("<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\"^^<http://www.w3.org/2001/XMLSchema#string> .", lines[0] );
-   }
-   
-   @Test
-   public void testBlankNodeContext() 
-   		throws RDFHandlerException
-   {
-       Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"), vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"), vf.createBNode());
-       
-       ByteArrayOutputStream baos = new ByteArrayOutputStream();
-       writer = rdfWriterFactory.getWriter(baos);
-       writer.startRDF();
-       writer.handleStatement(s1);
-       writer.endRDF();
-       
-       String content = baos.toString();
-       String[] lines = content.split("\n");
-       Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
-       Assert.assertTrue(lines[0].startsWith("<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\"^^<http://www.w3.org/2001/XMLSchema#string> _:"));
-   }	
+	}
+
+	@Test
+	public void testNoContext()
+		throws RDFHandlerException
+	{
+		Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"),
+				vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"));
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		writer = rdfWriterFactory.getWriter(baos);
+		writer.startRDF();
+		writer.handleStatement(s1);
+		writer.endRDF();
+
+		String content = baos.toString();
+		String[] lines = content.split("\n");
+		Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
+		Assert.assertEquals(
+				"<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\" .",
+				lines[0]);
+	}
+
+	@Test
+	public void testNoContextAddXSDString()
+		throws RDFHandlerException
+	{
+		Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"),
+				vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"));
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		writer = rdfWriterFactory.getWriter(baos);
+		writer.getWriterConfig().set(BasicWriterSettings.XSD_STRING_TO_PLAIN_LITERAL, false);
+		writer.startRDF();
+		writer.handleStatement(s1);
+		writer.endRDF();
+
+		String content = baos.toString();
+		String[] lines = content.split("\n");
+		Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
+		Assert.assertEquals(
+				"<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\"^^<http://www.w3.org/2001/XMLSchema#string> .",
+				lines[0]);
+	}
+
+	@Test
+	public void testBlankNodeContext()
+		throws RDFHandlerException
+	{
+		Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"),
+				vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"),
+				vf.createBNode());
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		writer = rdfWriterFactory.getWriter(baos);
+		writer.startRDF();
+		writer.handleStatement(s1);
+		writer.endRDF();
+
+		String content = baos.toString();
+		String[] lines = content.split("\n");
+		Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
+		Assert.assertTrue(lines[0].startsWith("<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\" _:"));
+	}
+
+	@Test
+	public void testBlankNodeContextAddXSDString()
+		throws RDFHandlerException
+	{
+		Statement s1 = vf.createStatement(vf.createURI("http://test.example.org/test/subject/1"),
+				vf.createURI("http://other.example.com/test/predicate/1"), vf.createLiteral("test literal"),
+				vf.createBNode());
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		writer = rdfWriterFactory.getWriter(baos);
+		writer.getWriterConfig().set(BasicWriterSettings.XSD_STRING_TO_PLAIN_LITERAL, false);
+		writer.startRDF();
+		writer.handleStatement(s1);
+		writer.endRDF();
+
+		String content = baos.toString();
+		String[] lines = content.split("\n");
+		Assert.assertEquals("Unexpected number of lines.", 1, lines.length);
+		Assert.assertTrue(lines[0].startsWith("<http://test.example.org/test/subject/1> <http://other.example.com/test/predicate/1> \"test literal\"^^<http://www.w3.org/2001/XMLSchema#string> _:"));
+	}
 }
