01e80f73c0c3e5444bb269e8bb6f851853049fe5
Peter Ansell <p_ansell@yahoo.com>
2013-11-01 13:05:57 +1100

    SES-1947 : Handle multiple calls to setStopAtFirstError correctly

--- core/rio/api/src/main/java/org/openrdf/rio/helpers/RDFParserBase.java
+++ core/rio/api/src/main/java/org/openrdf/rio/helpers/RDFParserBase.java
@@ -21,6 +21,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import info.aduna.net.ParsedURI;
@@ -235,9 +236,15 @@ public abstract class RDFParserBase implements RDFParser {
 	@Deprecated
 	@Override
 	public void setStopAtFirstError(boolean stopAtFirstError) {
-		if(!stopAtFirstError) {
-			getParserConfig().set(NTriplesParserSettings.FAIL_ON_NTRIPLES_INVALID_LINES, false);
+		getParserConfig().set(NTriplesParserSettings.FAIL_ON_NTRIPLES_INVALID_LINES, stopAtFirstError);
+		if (!stopAtFirstError) {
 			getParserConfig().addNonFatalError(NTriplesParserSettings.FAIL_ON_NTRIPLES_INVALID_LINES);
+		}
+		else {
+			// TODO: Add a ParserConfig.removeNonFatalError function to avoid this
+			Set<RioSetting<?>> set = new HashSet<RioSetting<?>>(getParserConfig().getNonFatalErrors());
+			set.remove(NTriplesParserSettings.FAIL_ON_NTRIPLES_INVALID_LINES);
+			getParserConfig().setNonFatalErrors(set);
 		}
 	}
 
