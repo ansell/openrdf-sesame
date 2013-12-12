f06f05d42828c4f29399574253cfd82d89381728
Jeen Broekstra <jeen.broekstra@gmail.com>
2013-12-12 14:06:44 +1300

    SES-1652 first steps for transaction handling over http.

---
+++ core/http/server-spring/src/main/java/org/openrdf/http/server/repository/transaction/TransactionStartController.java
@@ -0,0 +1,133 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.http.server.repository.transaction;
+
+import static javax.servlet.http.HttpServletResponse.SC_CREATED;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.glassfish.external.arc.Taxonomy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import info.aduna.webapp.views.SimpleResponseView;
+
+import org.openrdf.http.server.ClientHTTPException;
+import org.openrdf.http.server.ProtocolUtil;
+import org.openrdf.http.server.ServerHTTPException;
+import org.openrdf.http.server.repository.RepositoryInterceptor;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+/**
+ * Handles requests for transaction creation on a repository.
+ * 
+ * @since 2.8.0
+ * @author Jeen Broekstra
+ */
+public class TransactionStartController extends AbstractController {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public TransactionStartController()
+		throws ApplicationContextException
+	{
+		setSupportedMethods(new String[] { METHOD_POST });
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+		throws Exception
+	{
+		ModelAndView result;
+
+		Repository repository = RepositoryInterceptor.getRepository(request);
+
+		String reqMethod = request.getMethod();
+
+	  if (METHOD_POST.equals(reqMethod)) {
+			logger.info("POST transaction");
+			result = startTransaction(repository, request, response);
+			logger.info("POST transaction request finished.");
+		}
+		else {
+			throw new ClientHTTPException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Method not allowed: "
+					+ reqMethod);
+		}
+		return result;
+	}
+
+	
+	private UUID getTransactionID(HttpServletRequest request)
+		throws ClientHTTPException
+	{
+		String requestURL = request.getRequestURL().toString();
+
+		String queryString = request.getQueryString();
+
+		String pathInfoStr = request.getPathInfo();
+		logger.debug("path info: {}", pathInfoStr);
+
+		UUID txnID = null;
+
+		if (pathInfoStr != null && !pathInfoStr.equals("/")) {
+			String[] pathInfo = pathInfoStr.substring(1).split("/");
+			if (pathInfo.length > 0) {
+				txnID = UUID.fromString(pathInfo[3]); // FIXME test
+				logger.debug("txnID is '{}'", txnID);
+			}
+		}
+		
+		return txnID;
+	}
+
+	private ModelAndView startTransaction(Repository repository, HttpServletRequest request,
+			HttpServletResponse response)
+		throws IOException, ClientHTTPException, ServerHTTPException
+	{
+		ProtocolUtil.logRequestParameters(request);
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		try {
+			RepositoryConnection conn = repository.getConnection();
+			conn.begin();
+			UUID txnId = UUID.randomUUID();
+			
+			ActiveTransactionRegistry.register(txnId, conn);
+			model.put(SimpleResponseView.SC_KEY, SC_CREATED);
+			final StringBuffer txnURL = request.getRequestURL();
+			txnURL.append("/" + txnId.toString());
+			model.put(SimpleResponseView.CONTENT_KEY, txnURL.toString());
+			return new ModelAndView(SimpleResponseView.getInstance(), model);
+		}
+		catch (RepositoryException e) {
+			throw new ServerHTTPException("Transaction start error: " + e.getMessage(), e);
+		}
+	}
+
+}
