a09e87f81e907b85a05efb024a677864b4508054
Peter Ansell <p_ansell@yahoo.com>
2014-01-24 10:04:27 +1100

    SES-2004 : Add Namespaces.wrap(Set<Namespace>)
    This method can be used to insert the Set<Namespace> into API calls that
    require Map<String, String>, especially in cases where the API needs to
    insert new entries into the Map.

--- core/model/src/main/java/org/openrdf/model/util/Namespaces.java
+++ core/model/src/main/java/org/openrdf/model/util/Namespaces.java
@@ -16,11 +16,19 @@
  */
 package org.openrdf.model.util;
 
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.openrdf.model.Namespace;
+import org.openrdf.model.impl.NamespaceImpl;
 
 /**
  * A utility class to perform operations on {@link Namespace}s.
@@ -51,6 +59,141 @@ public class Namespaces {
 		}
 
 		return result;
+	}
+
+	/**
+	 * Wraps the given {@link Set} of {@link Namespace}s as a {@link Map} of
+	 * prefix to URI mappings, so that it can be used where a {@link Map} is
+	 * required by the API. <br>
+	 * NOTE: The Map returned by this method is not synchronized.
+	 * 
+	 * @param namespaces
+	 *        The Set to wrap.
+	 * @return A Map of prefix to URI mappings which is backed by the given Set
+	 *         of {@link Namespace}s.
+	 * @since 2.7.10
+	 */
+	public static Map<String, String> wrap(final Set<Namespace> namespaces) {
+		return new Map<String, String>() {
+
+			@Override
+			public void clear() {
+				namespaces.clear();
+			}
+
+			@Override
+			public boolean containsKey(Object nextKey) {
+				if (nextKey instanceof String) {
+					for (Namespace nextNamespace : namespaces) {
+						if (nextNamespace.getPrefix().equals(nextKey)) {
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public boolean containsValue(Object nextValue) {
+				if (nextValue instanceof String) {
+					for (Namespace nextNamespace : namespaces) {
+						if (nextNamespace.getName().equals(nextValue)) {
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+
+			/**
+			 * NOTE: This entry set is immutable, and does not update the internal
+			 * set through its iterator.
+			 */
+			@Override
+			public Set<java.util.Map.Entry<String, String>> entrySet() {
+				Set<java.util.Map.Entry<String, String>> result = new LinkedHashSet<Map.Entry<String, String>>();
+				for (Namespace nextNamespace : namespaces) {
+					AbstractMap.SimpleImmutableEntry<String, String> nextEntry = new SimpleImmutableEntry<String, String>(
+							nextNamespace.getPrefix(), nextNamespace.getName());
+					result.add(nextEntry);
+				}
+				return Collections.unmodifiableSet(result);
+			}
+
+			@Override
+			public String get(Object nextKey) {
+				if (nextKey instanceof String) {
+					for (Namespace nextNamespace : namespaces) {
+						if (nextNamespace.getPrefix().equals(nextKey)) {
+							return nextNamespace.getName();
+						}
+					}
+				}
+				return null;
+			}
+
+			@Override
+			public boolean isEmpty() {
+				return namespaces.isEmpty();
+			}
+
+			@Override
+			public Set<String> keySet() {
+				Set<String> result = new LinkedHashSet<String>();
+				for (Namespace nextNamespace : namespaces) {
+					result.add(nextNamespace.getPrefix());
+				}
+				return result;
+			}
+
+			@Override
+			public String put(String nextKey, String nextValue) {
+				String result = null;
+				for (Namespace nextNamespace : new LinkedHashSet<Namespace>(namespaces)) {
+					if (nextNamespace.getPrefix().equals(nextKey)) {
+						result = nextNamespace.getName();
+						namespaces.remove(nextNamespace);
+						namespaces.add(new NamespaceImpl(nextNamespace.getPrefix(), nextValue));
+						break;
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public void putAll(Map<? extends String, ? extends String> nextSet) {
+				for (Map.Entry<? extends String, ? extends String> nextEntry : nextSet.entrySet()) {
+					put(nextEntry.getKey(), nextEntry.getValue());
+				}
+			}
+
+			@Override
+			public String remove(Object nextKey) {
+				String result = null;
+				for (Namespace nextNamespace : new LinkedHashSet<Namespace>(namespaces)) {
+					if (nextNamespace.getPrefix().equals(nextKey)) {
+						result = nextNamespace.getName();
+						namespaces.remove(nextNamespace);
+						break;
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public int size() {
+				return namespaces.size();
+			}
+
+			@Override
+			public Collection<String> values() {
+				List<String> result = new ArrayList<String>();
+				for (Namespace nextNamespace : namespaces) {
+					result.add(nextNamespace.getName());
+				}
+				return result;
+			}
+		};
 	}
 
 	private Namespaces() {
