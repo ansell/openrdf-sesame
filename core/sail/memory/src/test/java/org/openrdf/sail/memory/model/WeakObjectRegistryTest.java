e963aa21217d85ede502fdfa991d931453198135
Peter Ansell <p_ansell@yahoo.com>
2014-02-14 16:05:58 +1100

    SES-1993 : Add unit test for WeakObjectRegistry

---
+++ core/sail/memory/src/test/java/org/openrdf/sail/memory/model/WeakObjectRegistryTest.java
@@ -0,0 +1,243 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.sail.memory.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link WeakObjectRegistry}.
+ * 
+ * @author Peter Ansell
+ */
+public class WeakObjectRegistryTest {
+
+	private WeakObjectRegistry<Object> wor;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp()
+		throws Exception
+	{
+		wor = new WeakObjectRegistry<Object>();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown()
+		throws Exception
+	{
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#size()}.
+	 */
+	@Ignore
+	@Test
+	public final void testSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#clear()}.
+	 */
+	@Ignore
+	@Test
+	public final void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#get(java.lang.Object)}
+	 * .
+	 */
+	@Ignore
+	@Test
+	public final void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#iterator()}.
+	 */
+	@Ignore
+	@Test
+	public final void testIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#contains(java.lang.Object)}
+	 * .
+	 */
+	@Ignore
+	@Test
+	public final void testContainsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#add(java.lang.Object)}
+	 * .
+	 */
+	@Ignore
+	@Test
+	public final void testAddE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openrdf.sail.memory.model.WeakObjectRegistry#remove(java.lang.Object)}
+	 * .
+	 */
+	@Ignore
+	@Test
+	public final void testRemoveObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractSet#removeAll(java.util.Collection)}.
+	 */
+	@Ignore
+	@Test
+	public final void testRemoveAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#toString()}.
+	 */
+	@Ignore
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#iterator()}.
+	 */
+	@Ignore
+	@Test
+	public final void testIterator1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#isEmpty()}.
+	 */
+	@Ignore
+	@Test
+	public final void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractCollection#contains(java.lang.Object)}.
+	 */
+	@Ignore
+	@Test
+	public final void testContainsObject1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#toArray()}.
+	 */
+	@Ignore
+	@Test
+	public final void testToArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#toArray(T[])}.
+	 */
+	@Ignore
+	@Test
+	public final void testToArrayTArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.AbstractCollection#add(java.lang.Object)}
+	 * .
+	 */
+	@Ignore
+	@Test
+	public final void testAddE1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractCollection#remove(java.lang.Object)}.
+	 */
+	@Ignore
+	@Test
+	public final void testRemoveObject1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractCollection#containsAll(java.util.Collection)}.
+	 */
+	@Ignore
+	@Test
+	public final void testContainsAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractCollection#addAll(java.util.Collection)}.
+	 */
+	@Ignore
+	@Test
+	public final void testAddAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link java.util.AbstractCollection#retainAll(java.util.Collection)}.
+	 */
+	@Ignore
+	@Test
+	public final void testRetainAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
