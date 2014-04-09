2e608ce4c689e9931c1ee25946fc0abc3fdb53b1
Jeen Broekstra <jeen.broekstra@gmail.com>
2014-04-09 14:14:14 +1200

    SES-1652 bumped protocol version to 7.

--- core/http/protocol/src/main/java/org/openrdf/http/protocol/Protocol.java
+++ core/http/protocol/src/main/java/org/openrdf/http/protocol/Protocol.java
@@ -38,7 +38,7 @@ public abstract class Protocol {
 	/**
 	 * Protocol version.
 	 */
-	public static final String VERSION = "6";
+	public static final String VERSION = "7";
 
 	/**
 	 * Parameter name for the 'subject' parameter of a statement query.
