ecec3f11b32b16dc28e98353f2fcc31d00a9c08f
Jeen Broekstra <jeen.broekstra@gmail.com>
2014-05-28 19:24:02 +1200

    SES-2052 added second unit for checking behaviour of IF when condition fails and 'else' expression is evaluated.

--- testsuites/sparql/src/main/java/org/openrdf/query/parser/sparql/ComplexSPARQLQueryTest.java
+++ testsuites/sparql/src/main/java/org/openrdf/query/parser/sparql/ComplexSPARQLQueryTest.java
@@ -832,8 +832,9 @@ public abstract class ComplexSPARQLQueryTest {
 		assertEquals(2, count);
 	}
 
-	@Test 
-	public void testSES2052If() throws Exception 
+	@Test
+	public void testSES2052If1()
+		throws Exception
 	{
 		loadTestData("/testdata-query/dataset-query.trig");
 		StringBuilder query = new StringBuilder();
@@ -842,7 +843,6 @@ public abstract class ComplexSPARQLQueryTest {
 		query.append("         ?s ?p ?o . \n");
 		query.append("        FILTER(IF(BOUND(?p), ?p = <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>, false)) \n");
 		query.append("}");
-		
 
 		TupleQuery tq = conn.prepareTupleQuery(QueryLanguage.SPARQL, query.toString());
 		try {
@@ -850,7 +850,7 @@ public abstract class ComplexSPARQLQueryTest {
 			assertNotNull(result);
 			while (result.hasNext()) {
 				BindingSet bs = result.next();
-				
+
 				URI p = (URI)bs.getValue("p");
 				assertNotNull(p);
 				assertEquals(RDF.TYPE, p);
@@ -862,7 +862,38 @@ public abstract class ComplexSPARQLQueryTest {
 		}
 
 	}
-	
+
+	@Test
+	public void testSES2052If2()
+		throws Exception
+	{
+		loadTestData("/testdata-query/dataset-query.trig");
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT ?p \n");
+		query.append("WHERE { \n");
+		query.append("         ?s ?p ?o . \n");
+		query.append("        FILTER(IF(!BOUND(?p), false , ?p = <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>)) \n");
+		query.append("}");
+
+		TupleQuery tq = conn.prepareTupleQuery(QueryLanguage.SPARQL, query.toString());
+		try {
+			TupleQueryResult result = tq.evaluate();
+			assertNotNull(result);
+			while (result.hasNext()) {
+				BindingSet bs = result.next();
+
+				URI p = (URI)bs.getValue("p");
+				assertNotNull(p);
+				assertEquals(RDF.TYPE, p);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	}
+
 	@Test
 	public void testSameTermRepeatInUnion()
 		throws Exception
@@ -1778,9 +1809,11 @@ public abstract class ComplexSPARQLQueryTest {
 	}
 
 	@Test
-	public void testSES2024PropertyPathAnonVarSharing() throws Exception {
+	public void testSES2024PropertyPathAnonVarSharing()
+		throws Exception
+	{
 		loadTestData("/testdata-query/dataset-ses2024.trig");
-		String query = "PREFIX : <http://example.org/> SELECT * WHERE { ?x1 :p/:lit ?l1 . ?x1 :diff ?x2 . ?x2 :p/:lit ?l2 . }" ;
+		String query = "PREFIX : <http://example.org/> SELECT * WHERE { ?x1 :p/:lit ?l1 . ?x1 :diff ?x2 . ?x2 :p/:lit ?l2 . }";
 
 		TupleQuery tq = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
 
