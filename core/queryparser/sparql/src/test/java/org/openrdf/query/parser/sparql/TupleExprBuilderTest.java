95cd9c484ff36863388eb497df7b96d81bca902c
Jeen Broekstra <jeen.broekstra@gmail.com>
2014-05-28 19:19:11 +1200

    SES-1809 moved unit test to SPARQLParserTest class.

--- core/queryparser/sparql/src/test/java/org/openrdf/query/parser/sparql/TupleExprBuilderTest.java
+++ core/queryparser/sparql/src/test/java/org/openrdf/query/parser/sparql/TupleExprBuilderTest.java
@@ -187,29 +187,6 @@ public class TupleExprBuilderTest {
 
 	}
 
-	@Test
-	public void testOrderByWithAliases1()
-		throws Exception
-	{
-		String queryString = " SELECT ?x (SUM(?v1)/COUNT(?v1) as ?r) WHERE { ?x <urn:foo> ?v1 } GROUP BY ?x ORDER BY ?r";
-
-		SPARQLParser parser = new SPARQLParser();
-		ParsedQuery query = parser.parseQuery(queryString, null);
-
-		assertNotNull(query);
-		TupleExpr te = query.getTupleExpr();
-
-		assertTrue(te instanceof Projection);
-
-		te = ((Projection)te).getArg();
-
-		assertTrue(te instanceof Order);
-
-		te = ((Order)te).getArg();
-
-		assertTrue(te instanceof Extension);
-	}
-
 	private class ServiceNodeFinder extends ASTVisitorBase {
 
 		private List<String> graphPatterns = new ArrayList<String>();
