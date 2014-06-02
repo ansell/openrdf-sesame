2056d9eed3b5573ae1730a9e96623d0a00e350ec
Dale Visser <dale.visser@live.com>
2014-06-02 18:02:41 -0400

    SES-2073 Added template.ts, create-federate.ts, create.ts, paging.ts          jquery.d.ts (from DefinitelyTyped). All of this is still untested.

---
+++ core/http/workbench/src/main/webapp/scripts/template.ts
@@ -0,0 +1,99 @@
+module workbench {
+
+    export interface LoadRoutine {
+        (ev?: Event): void;
+    }
+
+    interface LoadRoutineArray {
+        [index: number]: LoadRoutine;
+        length: number;
+    }
+
+    // The following is to allow composed XSLT style sheets to each add
+    // functions to the window.onload event.
+    function chain(args: LoadRoutineArray): LoadRoutine {
+            return function() {
+            for (var i = 0; i < args.length; i++) {
+                args[i]();
+            }
+        }
+    }
+
+    // Note that the way this is currently constructed, functions added with
+    // addLoad() will be executed in the order that they were added.
+    //
+    // @see
+    // http://onwebdevelopment.blogspot.com/2008/07/chaining-functions-in-javascript.html
+    // @param fn
+    // function to add
+    export function addLoad(fn: LoadRoutine) {
+        window.onload = typeof (window.onload) == 'function' ? chain([
+            window.onload, fn]) : fn;
+    }
+
+    /**
+     * Retrieves the value of the cookie with the given name.
+     * 
+     * @param {String}
+     *            name The name of the cookie to retrieve.
+     * @returns {String} The value of the given cookie, or an empty string if it
+     *          doesn't exist.
+     */
+    export function getCookie(name: string): string {
+        var cookies = document.cookie.split(';');
+        var rval = '';
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eq = cookie.indexOf('=');
+            if (name == cookie.substr(0, eq).replace(/^\s+|\s+$/g, '')) {
+                rval = decodeURIComponent(cookie.substr(eq + 1).replace(/\+/g,
+                    '%20'));
+                break;
+            }
+        }
+        return rval;
+    }
+
+    /**
+     * Parses workbench URL query strings into processable arrays.
+     * 
+     * @returns an array of the 'name=value' substrings of the URL query string
+     */
+    export function getQueryStringElements(): string[] {
+        var href = document.location.href;
+        return href.substring(href.indexOf('?') + 1).split(
+            decodeURIComponent('%26'));
+    }
+
+    /**
+     * Utility method for assembling the query string for a request URL.
+     * 
+     * @param sb
+     *            string buffer, actually an array of strings to be joined later
+     * @param id
+     *            name of parameter to add, also the id of the document element
+     *            to get the value from
+     */
+    export function addParam(sb: string[], id: string): void {
+        sb[sb.length] = id + '=';
+        var tag = document.getElementById(id);
+        sb[sb.length] = (tag.getAttribute('type') == 'checkbox') ?
+        tag.getAttribute('checked') : encodeURIComponent(tag.innerText);
+        sb[sb.length] = '&';
+    }
+}
+
+/**
+ * Code to run when the document loads: eliminate the 'noscript' warning
+ * message, and display an unauthenticated user properly.
+ */
+workbench
+    .addLoad(function() {
+        document.getElementById('noscript-message').style.display = 'none';
+        var user = workbench.getCookie('server-user');
+        if (user.length == 0 || user == '""') {
+            user = '<span class="disabled">None</span>';
+        }
+        var selectedUser = document.getElementById('selected-user');
+        selectedUser.innerHTML = user;
+    });
