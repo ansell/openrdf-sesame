d3f056729c83c4aba7c598a290e21c3d43471bb7
Anton Marsden <anton.marsden@insightng.com>
2014-11-03 16:48:13 +1300

    Simplified buffer allocation code

--- core/rio/binary/src/main/java/org/openrdf/rio/binary/BinaryRDFParser.java
+++ core/rio/binary/src/main/java/org/openrdf/rio/binary/BinaryRDFParser.java
@@ -282,9 +282,8 @@ public class BinaryRDFParser extends RDFParserBase {
 		int stringLength = in.readInt();
 		int stringBytes = stringLength << 1;
 		if (buf == null || buf.length < stringBytes) {
-			// Always grow the buffer to at least double its previous size
-			int newCap = (buf == null ? stringBytes : Math.max(stringBytes, buf.length << 1));
-			buf = new byte[newCap];
+			// Allocate what we need plus some extra space
+			buf = new byte[stringBytes << 1];
 		}
 		in.readFully(buf, 0, stringBytes);
 		return new String(buf, 0, stringBytes, "UTF-16BE");
