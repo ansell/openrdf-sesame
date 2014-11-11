d3be1019c70426a1d143c31add2662e3a6f903ca
Dale Visser <dvisser@ida.org>
2014-11-11 10:32:21 -0500

    SES-2125 Added passing test cases for the simple expected namespace behaviors.

---
+++ core/sail/federation/src/test/java/org/openrdf/sail/federation/FederationNamespacesTest.java
@@ -0,0 +1,110 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.sail.federation;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.sail.memory.MemoryStore;
+
+
+/**
+ * Tests for correct behavior when members define matching or conflicting
+ * prefix/namespace maps.
+ * 
+ * @author Dale Visser
+ */
+public class FederationNamespacesTest extends TestCase {
+	
+	@Test
+	public void testTwoMatchingNamespaces()
+		throws RepositoryException, RDFParseException, IOException{
+		Federation federation = new Federation();
+		SailRepository repo = new SailRepository(federation);
+		repo.initialize();
+		federation.addMember(createMember("1", "a", "http://test/a#"));
+		federation.addMember(createMember("2", "a", "http://test/a#"));
+		RepositoryConnection con = repo.getConnection();
+		try {
+		assertThat(con.getNamespace("a"), is(equalTo("http://test/a#")));
+		} finally {
+			con.close();
+		}
+	}
+
+	@Test
+	public void testThreeMatchingNamespaces()
+		throws RepositoryException, RDFParseException, IOException{
+		Federation federation = new Federation();
+		SailRepository repo = new SailRepository(federation);
+		repo.initialize();
+		federation.addMember(createMember("1", "a", "http://test/a#"));
+		federation.addMember(createMember("2", "a", "http://test/a#"));
+		federation.addMember(createMember("3", "a", "http://test/a#"));
+		RepositoryConnection con = repo.getConnection();
+		try {
+		assertThat(con.getNamespace("a"), is(equalTo("http://test/a#")));
+		} finally {
+			con.close();
+		}
+	}
+	
+	@Test
+	public void testTwoMismatchedNamespaces()
+		throws RepositoryException, RDFParseException, IOException{
+		Federation federation = new Federation();
+		SailRepository repo = new SailRepository(federation);
+		repo.initialize();
+		federation.addMember(createMember("1", "a", "http://test/a#"));
+		federation.addMember(createMember("2", "a", "http://test/b#"));
+		RepositoryConnection con = repo.getConnection();
+		try {
+		assertThat(con.getNamespace("a"), is(nullValue()));
+		} finally {
+			con.close();
+		}
+	}	
+	
+	
+	private Repository createMember(String memberID, String prefix, String name)
+			throws RepositoryException, RDFParseException, IOException {
+		SailRepository member = new SailRepository(new MemoryStore());
+		member.initialize();
+		SailRepositoryConnection con = member.getConnection();
+		try {
+			con.setNamespace(prefix, name);
+		} finally {
+			con.close();
+		}
+		return member;
+	}
+
+}
