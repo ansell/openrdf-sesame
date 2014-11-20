5f75d53b117d0f12277c74b1cebd85e8a0fcadd9
Jeen Broekstra <jeen.broekstra@gmail.com>
2014-11-21 10:45:00 +1300

    SES-2136 cleaned up and re-enabled Sesame-specific manifest test case.

---
+++ compliance/sparql/src/test/java/org/openrdf/query/parser/sparql/manifest/SesameExtraSPARQL11QueryTest.java
@@ -0,0 +1,74 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.query.parser.sparql.manifest;
+
+import junit.framework.Test;
+
+import org.openrdf.query.Dataset;
+import org.openrdf.query.parser.sparql.manifest.SPARQL11ManifestTest;
+import org.openrdf.query.parser.sparql.manifest.SPARQLQueryTest;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.dataset.DatasetRepository;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.sail.memory.MemoryStore;
+
+/**
+ * Additional Manifest-based test cases for SPARQL 1.1 querying (see
+ * sesame-sparql-testsuite/src/main/resources/testcases-sparql-1.1)
+ *
+ * @author Jeen Broekstra
+ */
+public class SesameExtraSPARQL11QueryTest extends SPARQLQueryTest {
+
+	public static Test suite()
+		throws Exception
+	{
+		return SPARQL11ManifestTest.suite(new Factory() {
+
+			public SesameExtraSPARQL11QueryTest createSPARQLQueryTest(String testURI, String name,
+					String queryFileURL, String resultFileURL, Dataset dataSet, boolean laxCardinality)
+			{
+				return createSPARQLQueryTest(testURI, name, queryFileURL, resultFileURL, dataSet, laxCardinality,
+						false);
+			}
+
+			public SesameExtraSPARQL11QueryTest createSPARQLQueryTest(String testURI, String name,
+					String queryFileURL, String resultFileURL, Dataset dataSet, boolean laxCardinality,
+					boolean checkOrder)
+			{
+				return new SesameExtraSPARQL11QueryTest(testURI, name, queryFileURL, resultFileURL, dataSet,
+						laxCardinality, checkOrder);
+			}
+		}, false, false, false);
+	}
+
+	protected SesameExtraSPARQL11QueryTest(String testURI, String name, String queryFileURL,
+			String resultFileURL, Dataset dataSet, boolean laxCardinality)
+	{
+		this(testURI, name, queryFileURL, resultFileURL, dataSet, laxCardinality, false);
+	}
+
+	protected SesameExtraSPARQL11QueryTest(String testURI, String name, String queryFileURL,
+			String resultFileURL, Dataset dataSet, boolean laxCardinality, boolean checkOrder)
+	{
+		super(testURI, name, queryFileURL, resultFileURL, dataSet, laxCardinality, checkOrder);
+	}
+
+	protected Repository newRepository() {
+		return new DatasetRepository(new SailRepository(new MemoryStore()));
+	}
+}
