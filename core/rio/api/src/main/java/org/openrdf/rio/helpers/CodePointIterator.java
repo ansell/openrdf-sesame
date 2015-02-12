b697aa9ad941349666f0dce50594e0c4c8aead92
Jeen Broekstra <jeen.broekstra@gmail.com>
2015-02-12 15:46:42 +1300

    SES-2166 manual backporting of UTF-8 codepoints fixes for the 2.7 branch

---
+++ core/rio/api/src/main/java/org/openrdf/rio/helpers/CodePointIterator.java
@@ -0,0 +1,56 @@
+/* 
+ * Licensed to Aduna under one or more contributor license agreements.  
+ * See the NOTICE.txt file distributed with this work for additional 
+ * information regarding copyright ownership. 
+ *
+ * Aduna licenses this file to you under the terms of the Aduna BSD 
+ * License (the "License"); you may not use this file except in compliance 
+ * with the License. See the LICENSE.txt file distributed with this work 
+ * for the full License.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.openrdf.rio.helpers;
+
+import java.util.Iterator;
+
+/**
+ * Helper class for quickly iterating over a String and receiving each character
+ * code point (taking care to handle surrogate pairs correctly).
+ * 
+ * @author Jeen Broekstra
+ * @since 2.8.0
+ * @see CodePointSequence
+ */
+public class CodePointIterator implements Iterator<Integer> {
+
+	private final String source;
+
+	private int index = 0;
+
+	public CodePointIterator(String source) {
+		this.source = source;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return index < source.length();
+	}
+
+	@Override
+	public Integer next() {
+		int codePoint = source.codePointAt(index);
+		index += Character.charCount(codePoint);
+		return codePoint;
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
--- core/rio/api/src/main/java/org/openrdf/rio/helpers/RDFParserHelper.java
+++ core/rio/api/src/main/java/org/openrdf/rio/helpers/RDFParserHelper.java
@@ -413,8 +413,8 @@ public class RDFParserHelper {
 	/**
 	 * Reports a fatal error to the registered ParseErrorListener, if any, and
 	 * throws a <tt>ParseException</tt> afterwards. This method simply calls
-	 * {@link #reportFatalError(String,int,int)} supplying <tt>-1</tt> for the
-	 * line- and column number.
+	 * {@link #reportFatalError(String, int, int, ParseErrorListener)} supplying
+	 * <tt>-1</tt> for the line- and column number.
 	 * 
 	 * @since 2.7.1
 	 */
@@ -449,7 +449,8 @@ public class RDFParserHelper {
 	 * the error message is not reported to the ParseErrorListener, assuming that
 	 * it has already been reported when the original ParseException was thrown.
 	 * <p>
-	 * This method simply calls {@link #reportFatalError(Exception,int,int)}
+	 * This method simply calls
+	 * {@link #reportFatalError(Exception, int, int, ParseErrorListener)}
 	 * supplying <tt>-1</tt> for the line- and column number.
 	 * 
 	 * @since 2.7.1
