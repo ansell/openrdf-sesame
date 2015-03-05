acfd6dfe9f2561cdd742df11473d3d40528c7532
Dale Visser <dvisser@ida.org>
2015-03-05 11:31:57 -0500

    SES-2057 Simplify UpdateServlet client-side code to only POST SPARQL/Update.

--- core/http/workbench/src/main/webapp/scripts/update.js
+++ core/http/workbench/src/main/webapp/scripts/update.js
@@ -21,46 +21,42 @@ var workbench;
                     if (args.update)
                         yasqe.setValue(args.update);
                 },
+                // This way, we don't conflict with the YASQE editor of the
+                // regular query interface, and we show the most recent
+                // -update- query.
                 persistent: "update"
             });
 
-            //some styling conflicts. Could add my own css file, but not a lot of things need changing, so just do this programmatically
-            //first, set the font size (otherwise font is as small as menu, which is too small)
-            //second, set the width. YASQE normally expands to 100%, but the use of a table requires us to set a fixed width
-            $(yasqe.getWrapperElement()).css({ "fontSize": "14px", "width": "900px" });
+            // Some styling conflicts. Could add my own css file, but not a
+            // lot of things need changing, so just do this programmatically.
+            // First, set the font size (otherwise font is as small as menu,
+            // which is too small). Second, set the width. YASQE normally
+            // expands to 100%, but the use of a table requires us to set a
+            // fixed width.
+            $(yasqe.getWrapperElement()).css({
+                "fontSize": "14px",
+                "width": "900px"
+            });
 
-            //we made a change to the css wrapper element (and did so after initialization). So, force a manual update of the yasqe instance
+            // We made a change to the css wrapper element (and did so after
+            // initialization). So, force a manual update of the yasqe
+            // instance.
             yasqe.refresh();
 
-            //if the text area we instantiated YASQE on has no query val, then show a regular default update query
-            if (yasqe.getValue().trim().length == 0)
-                yasqe.setValue("INSERT DATA {<http://exampleSub> <http://examplePred> <http://exampleObj>}");
+            // If the text area we instantiated YASQE on has no query val,
+            // then show a regular default update query.
+            if (yasqe.getValue().trim().length == 0) {
+                yasqe.setValue('INSERT DATA {\n\t<http://exampleSub> ' + '<http://examplePred> <http://exampleObj> .\n}');
+            }
         }
         _update.initYasqe = initYasqe;
+
         function doSubmit() {
-            //save yasqe content to textarea
-            if (yasqe)
+            // Save yasqe content to text area.
+            if (yasqe) {
                 yasqe.save();
-            if ($('#update').text().length >= 1000) {
-                // Too long to put in URL for a GET request. Instead, POST.
-                // Browser back-button may not work as expected.
-                return true;
-            } else {
-                var url = [];
-                url[url.length] = 'update';
-                if (document.all) {
-                    url[url.length] = ';';
-                } else {
-                    url[url.length] = '?';
-                }
-                workbench.addParam(url, 'queryLn');
-                workbench.addParam(url, 'update');
-                workbench.addParam(url, 'limit');
-                workbench.addParam(url, 'infer');
-                url[url.length - 1] = '';
-                document.location.href = url.join('');
-                return false;
             }
+            return true;
         }
         _update.doSubmit = doSubmit;
     })(workbench.update || (workbench.update = {}));
