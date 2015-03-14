66dc5d7ddc0d5cb2d22c6ffcd5f019b909f7c7f9
Peter Ansell <p_ansell@yahoo.com>
2015-03-15 09:49:30 +1100

    SES-1704 : Add @Override to CloseableInteration.close
    Also change javadoc to recommend try-with-resources, which can be used
    with any AutoCloseable

--- core/util/src/main/java/info/aduna/iteration/CloseableIteration.java
+++ core/util/src/main/java/info/aduna/iteration/CloseableIteration.java
@@ -21,19 +21,16 @@ package info.aduna.iteration;
  * An {@link Iteration} that can be closed to free resources that it is holding.
  * CloseableIterations automatically free their resources when exhausted. If not
  * read until exhaustion or if you want to make sure the iteration is properly
- * closed, any code using the iterator should be placed in a try-catch-finally
+ * closed, any code using the iterator should be placed in a try-with-resources
  * block, closing the iteration in the finally, e.g.:
  * 
  * <pre>
- * CloseableIteration&lt;Object, Exception&gt; iter = ...
- * try {
+ * 
+ * try (CloseableIteration&lt;Object, Exception&gt; iter = ...) {
  *    // read objects from the iterator
  * }
  * catch(Exception e) {
  *   // process the exception that can be thrown while processing.
- * }
- * finally {
- *    iter.close();
  * }
  * </pre>
  */
@@ -43,6 +40,7 @@ public interface CloseableIteration<E, X extends Exception> extends Iteration<E,
 	 * Closes this iteration, freeing any resources that it is holding. If the
 	 * iteration has already been closed then invoking this method has no effect.
 	 */
+	@Override
 	public void close()
 		throws X;
 
