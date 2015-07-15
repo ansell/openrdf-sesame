0672d477d3bd8afc626241ac39107b1d62535ff0
Mark <mark.hale@physics.org>
2015-07-15 22:08:33 +0100

    eh

---
+++ core/queryalgebra/geosparql/src/main/java/org/openrdf/query/algebra/evaluation/function/geosparql/EhEquals.java
@@ -0,0 +1,22 @@
+package org.openrdf.query.algebra.evaluation.function.geosparql;
+
+import org.openrdf.model.vocabulary.GEOF;
+import org.openrdf.query.algebra.evaluation.function.Function;
+
+import com.spatial4j.core.shape.Shape;
+
+/**
+ * The GeoSPARQL {@link Function} geof:ehEquals,
+ * as defined in <a href="http://www.opengeospatial.org/standards/geosparql">OGC GeoSPARQL - A Geographic Query Language for RDF Data</a>.
+ */
+public class EhEquals extends GeometricRelationFunction {
+	@Override
+	public String getURI() {
+		return GEOF.EH_EQUALS.stringValue();
+	}
+
+	@Override
+	protected boolean relation(Shape s1, Shape s2) {
+		return SpatialSupport.getSpatialAlgebra().equals(s1, s2);
+	}
+}
